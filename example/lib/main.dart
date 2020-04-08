3f530fa1932622b437954b9f919d98043cbeb180
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2020-04-08 13:31:39 +0200

    fix example main

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.system,
       home: Container(
         child: StreamChat(
           client: client,
