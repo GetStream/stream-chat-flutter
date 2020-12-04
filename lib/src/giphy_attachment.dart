44cd3754fb51fa93c859a15f1ef48e74e1e7d294
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2020-12-04 15:23:25 +0100

    fix giphy badge

--- lib/src/giphy_attachment.dart
+++ lib/src/giphy_attachment.dart
@@ -103,33 +103,30 @@ class GiphyAttachment extends StatelessWidget {
                     ),
                   ),
                   Positioned(
-                    left: 0,
-                    top: 0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            bottomRight: Radius.circular(16.0),
-                          )),
+                    bottom: 16,
+                    left: 16,
+                    child: Material(
+                      color: Colors.black.withOpacity(.5),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
                       child: Padding(
-                        padding: const EdgeInsets.only(
-                          left: 8.0,
-                          right: 8.0,
-                          top: 8.0,
-                          bottom: 4.0,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0,
+                          vertical: 4.0,
                         ),
                         child: Row(
                           children: [
                             StreamSvgIcon.lightning(
-                              color: StreamChatTheme.of(context).accentColor,
-                              size: 16.0,
+                              color: Colors.white,
+                              size: 16,
                             ),
                             Text(
                               'GIPHY',
                               style: TextStyle(
-                                color: StreamChatTheme.of(context).accentColor,
+                                color: Colors.white,
                                 fontWeight: FontWeight.bold,
-                                fontSize: 11.0,
+                                fontSize: 11,
                               ),
                             ),
                           ],
