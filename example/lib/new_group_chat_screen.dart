7ae15263e335babbb15e87a80e9eec7c3ef8f663
Sahil Kumar <xdsahil@gmail.com>
2021-01-06 19:07:51 +0530

    [NewGroupChatScreen] Refactor
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- example/lib/new_group_chat_screen.dart
+++ example/lib/new_group_chat_screen.dart
@@ -52,234 +52,227 @@ class _NewGroupChatScreenState extends State<NewGroupChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: StreamChatTheme.of(context).colorTheme.whiteSnow,
-      body: SafeArea(
-        child: NestedScrollView(
-          floatHeaderSlivers: true,
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                brightness: Theme.of(context).brightness,
-                pinned: true,
-                forceElevated: true,
-                elevation: 1,
-                backgroundColor: StreamChatTheme.of(context).colorTheme.white,
-                leading: const StreamBackButton(),
-                title: Text(
-                  'Add Group Members',
-                  style: TextStyle(
-                    color: StreamChatTheme.of(context).colorTheme.black,
-                    fontSize: 16,
-                  ),
-                ),
-                centerTitle: true,
-                actions: [
-                  if (_selectedUsers.isNotEmpty)
-                    IconButton(
-                      icon: StreamSvgIcon.arrow_right(
-                        color:
-                            StreamChatTheme.of(context).colorTheme.accentBlue,
-                      ),
-                      onPressed: () async {
-                        final updatedList = await Navigator.pushNamed(
-                          context,
-                          Routes.NEW_GROUP_CHAT_DETAILS,
-                          arguments: _selectedUsers.toList(growable: false),
-                        );
-                        if (updatedList != null) {
-                          setState(() {
-                            _selectedUsers
-                              ..clear()
-                              ..addAll(updatedList);
-                          });
-                        }
-                      },
-                    )
-                ],
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: StreamChatTheme.of(context).colorTheme.white,
+        leading: const StreamBackButton(),
+        title: Text(
+          'Add Group Members',
+          style: TextStyle(
+            color: StreamChatTheme.of(context).colorTheme.black,
+            fontSize: 16,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          if (_selectedUsers.isNotEmpty)
+            IconButton(
+              icon: StreamSvgIcon.arrow_right(
+                color: StreamChatTheme.of(context).colorTheme.accentBlue,
               ),
+              onPressed: () async {
+                final updatedList = await Navigator.pushNamed(
+                  context,
+                  Routes.NEW_GROUP_CHAT_DETAILS,
+                  arguments: _selectedUsers.toList(growable: false),
+                );
+                if (updatedList != null) {
+                  setState(() {
+                    _selectedUsers
+                      ..clear()
+                      ..addAll(updatedList);
+                  });
+                }
+              },
+            )
+        ],
+      ),
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverToBoxAdapter(
+              child: SearchTextField(
+                controller: _controller,
+              ),
+            ),
+            if (_selectedUsers.isNotEmpty)
               SliverToBoxAdapter(
-                child: SearchTextField(
-                  controller: _controller,
-                ),
-              ),
-              if (_selectedUsers.isNotEmpty)
-                SliverToBoxAdapter(
-                  child: Container(
-                    height: 104,
-                    child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: _selectedUsers.length,
-                      padding: const EdgeInsets.all(8),
-                      separatorBuilder: (_, __) => SizedBox(width: 16),
-                      itemBuilder: (_, index) {
-                        final user = _selectedUsers.elementAt(index);
-                        return Column(
-                          children: [
-                            Stack(
-                              children: [
-                                UserAvatar(
-                                  onlineIndicatorAlignment: Alignment(0.9, 0.9),
-                                  user: user,
-                                  showOnlineStatus: true,
-                                  borderRadius: BorderRadius.circular(32),
-                                  constraints: BoxConstraints.tightFor(
-                                    height: 64,
-                                    width: 64,
-                                  ),
+                child: Container(
+                  height: 104,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: _selectedUsers.length,
+                    padding: const EdgeInsets.all(8),
+                    separatorBuilder: (_, __) => SizedBox(width: 16),
+                    itemBuilder: (_, index) {
+                      final user = _selectedUsers.elementAt(index);
+                      return Column(
+                        children: [
+                          Stack(
+                            children: [
+                              UserAvatar(
+                                onlineIndicatorAlignment: Alignment(0.9, 0.9),
+                                user: user,
+                                showOnlineStatus: true,
+                                borderRadius: BorderRadius.circular(32),
+                                constraints: BoxConstraints.tightFor(
+                                  height: 64,
+                                  width: 64,
                                 ),
-                                Positioned(
-                                  top: -4,
-                                  right: -4,
-                                  child: GestureDetector(
-                                    onTap: () {
-                                      if (_selectedUsers.contains(user)) {
-                                        setState(
-                                            () => _selectedUsers.remove(user));
-                                      }
-                                    },
-                                    child: Container(
-                                      decoration: BoxDecoration(
+                              ),
+                              Positioned(
+                                top: -4,
+                                right: -4,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    if (_selectedUsers.contains(user)) {
+                                      setState(
+                                          () => _selectedUsers.remove(user));
+                                    }
+                                  },
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: StreamChatTheme.of(context)
+                                          .colorTheme
+                                          .white,
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
                                         color: StreamChatTheme.of(context)
                                             .colorTheme
-                                            .white,
-                                        shape: BoxShape.circle,
-                                        border: Border.all(
-                                          color: StreamChatTheme.of(context)
-                                              .colorTheme
-                                              .whiteSnow,
-                                        ),
-                                      ),
-                                      child: StreamSvgIcon.close(
-                                        color: StreamChatTheme.of(context)
-                                            .colorTheme
-                                            .black,
-                                        size: 24,
+                                            .whiteSnow,
                                       ),
                                     ),
-                                  ),
-                                )
-                              ],
-                            ),
-                            SizedBox(height: 4),
-                            Text(
-                              user.name.split(' ')[0],
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 12,
-                              ),
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              SliverPersistentHeader(
-                pinned: true,
-                delegate: _HeaderDelegate(
-                  height: 30,
-                  child: Container(
-                    width: double.maxFinite,
-                    decoration: BoxDecoration(
-                      gradient:
-                          StreamChatTheme.of(context).colorTheme.bgGradient,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 8,
-                        horizontal: 8,
-                      ),
-                      child: Text(
-                        _isSearchActive
-                            ? 'Matches for \"$_userNameQuery\"'
-                            : 'On the platform',
-                        style: TextStyle(
-                          color: StreamChatTheme.of(context).colorTheme.grey,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ];
-          },
-          body: GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onPanDown: (_) => FocusScope.of(context).unfocus(),
-            child: UsersBloc(
-              child: UserListView(
-                selectedUsers: _selectedUsers,
-                pullToRefresh: false,
-                groupAlphabetically: _isSearchActive ? false : true,
-                onUserTap: (user, _) {
-                  if (!_selectedUsers.contains(user)) {
-                    setState(() {
-                      _selectedUsers.add(user);
-                    });
-                  } else {
-                    setState(() {
-                      _selectedUsers.remove(user);
-                    });
-                  }
-                },
-                pagination: PaginationParams(
-                  limit: 25,
-                ),
-                filter: {
-                  if (_userNameQuery.isNotEmpty)
-                    'name': {
-                      r'$autocomplete': _userNameQuery,
-                    },
-                  'id': {
-                    r'$ne': StreamChat.of(context).user.id,
-                  }
-                },
-                sort: [
-                  SortOption(
-                    'name',
-                    direction: 1,
-                  ),
-                ],
-                emptyBuilder: (_) {
-                  return LayoutBuilder(
-                    builder: (context, viewportConstraints) {
-                      return SingleChildScrollView(
-                        physics: AlwaysScrollableScrollPhysics(),
-                        child: ConstrainedBox(
-                          constraints: BoxConstraints(
-                            minHeight: viewportConstraints.maxHeight,
-                          ),
-                          child: Center(
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(24),
-                                  child: StreamSvgIcon.search(
-                                    size: 96,
-                                    color: StreamChatTheme.of(context)
-                                        .colorTheme
-                                        .grey,
+                                    child: StreamSvgIcon.close(
+                                      color: StreamChatTheme.of(context)
+                                          .colorTheme
+                                          .black,
+                                      size: 24,
+                                    ),
                                   ),
                                 ),
-                                Text(
-                                  'No user matches these keywords...',
-                                  style: StreamChatTheme.of(context)
-                                      .textTheme
-                                      .footnote
-                                      .copyWith(
-                                        color: StreamChatTheme.of(context)
-                                            .colorTheme
-                                            .grey,
-                                      ),
-                                ),
-                              ],
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            user.name.split(' ')[0],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
                             ),
                           ),
-                        ),
+                        ],
                       );
                     },
-                  );
-                },
+                  ),
+                ),
               ),
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: _HeaderDelegate(
+                height: 30,
+                child: Container(
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    gradient: StreamChatTheme.of(context).colorTheme.bgGradient,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 8,
+                    ),
+                    child: Text(
+                      _isSearchActive
+                          ? 'Matches for \"$_userNameQuery\"'
+                          : 'On the platform',
+                      style: TextStyle(
+                        color: StreamChatTheme.of(context).colorTheme.grey,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ];
+        },
+        body: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onPanDown: (_) => FocusScope.of(context).unfocus(),
+          child: UsersBloc(
+            child: UserListView(
+              selectedUsers: _selectedUsers,
+              pullToRefresh: false,
+              groupAlphabetically: _isSearchActive ? false : true,
+              onUserTap: (user, _) {
+                if (!_selectedUsers.contains(user)) {
+                  setState(() {
+                    _selectedUsers.add(user);
+                  });
+                } else {
+                  setState(() {
+                    _selectedUsers.remove(user);
+                  });
+                }
+              },
+              pagination: PaginationParams(
+                limit: 25,
+              ),
+              filter: {
+                if (_userNameQuery.isNotEmpty)
+                  'name': {
+                    r'$autocomplete': _userNameQuery,
+                  },
+                'id': {
+                  r'$ne': StreamChat.of(context).user.id,
+                }
+              },
+              sort: [
+                SortOption(
+                  'name',
+                  direction: 1,
+                ),
+              ],
+              emptyBuilder: (_) {
+                return LayoutBuilder(
+                  builder: (context, viewportConstraints) {
+                    return SingleChildScrollView(
+                      physics: AlwaysScrollableScrollPhysics(),
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minHeight: viewportConstraints.maxHeight,
+                        ),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(24),
+                                child: StreamSvgIcon.search(
+                                  size: 96,
+                                  color: StreamChatTheme.of(context)
+                                      .colorTheme
+                                      .grey,
+                                ),
+                              ),
+                              Text(
+                                'No user matches these keywords...',
+                                style: StreamChatTheme.of(context)
+                                    .textTheme
+                                    .footnote
+                                    .copyWith(
+                                      color: StreamChatTheme.of(context)
+                                          .colorTheme
+                                          .grey,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
             ),
           ),
         ),
