d4b0ab87e109f8a14b68ff9aa879dc167475986a
Neevash Ramdial <mail@neevash.dev>
2021-01-15 12:37:01 -0400

    ignore deprecated api

--- packages/flutter_widgets/lib/src/utils.dart
+++ packages/flutter_widgets/lib/src/utils.dart
@@ -9,7 +9,8 @@ Future<void> launchURL(BuildContext context, String url) async {
   if (await canLaunch(url)) {
     await launch(url);
   } else {
-    ScaffoldMessenger.of(context).showSnackBar(
+    // ignore: deprecated_member_use
+    Scaffold.of(context).showSnackBar(
       SnackBar(
         content: Text('Cannot launch the url'),
       ),
