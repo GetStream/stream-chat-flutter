4b9a7d68372d272e52e117c306f9631e5e97d818
Sahil Kumar <xdsahil@gmail.com>
2021-01-29 15:22:19 +0530

    [Core -> LazyLoadScrollView] Remove `isLoading` property
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- packages/stream_chat_flutter_core/lib/src/lazy_load_scroll_view.dart
+++ packages/stream_chat_flutter_core/lib/src/lazy_load_scroll_view.dart
@@ -27,9 +27,6 @@ class LazyLoadScrollView extends StatefulWidget {
   /// The offset to take into account when triggering [onEndOfPage]/[onStartOfPage] in pixels
   final double scrollOffset;
 
-  /// Used to determine if loading of new data has finished. You should use set this if you aren't using a FutureBuilder or StreamBuilder
-  final bool isLoading;
-
   /// Initiates a LazyLoadScrollView widget
   const LazyLoadScrollView({
     Key key,
@@ -39,7 +36,6 @@ class LazyLoadScrollView extends StatefulWidget {
     this.onPageScrollStart,
     this.onPageScrollEnd,
     this.onInBetweenOfPage,
-    this.isLoading = false,
     this.scrollOffset = 100,
   })  : assert(child != null),
         super(key: key);
