b08940bd61d5eaf3ea094af639bc095aea6f55c4
Neevash Ramdial <mail@neevash.dev>
2021-01-29 10:18:41 -0400

    extract typedef to file

---
+++ packages/stream_chat_flutter_core/lib/src/typedef.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+/// A signature for a callback which exposes an error and returns a function.
+/// This Callback can be used in cases where an API failure occurs and the widget
+/// is unable to render data.
+typedef ErrorBuilder = Widget Function(
+    BuildContext context, Object error);
