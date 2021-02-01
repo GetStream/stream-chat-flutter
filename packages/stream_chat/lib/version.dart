38a1b880e54a668dc9420576ccf14c91d1541d66
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2021-02-01 16:11:29 +0100

    update deps

--- packages/stream_chat/lib/version.dart
+++ packages/stream_chat/lib/version.dart
@@ -2,4 +2,4 @@ import 'package:stream_chat/src/client.dart';
 
 /// Current package version
 /// Used in [StreamChatClient] to build the `x-stream-client` header
-const PACKAGE_VERSION = '0.2.24+2';
+const PACKAGE_VERSION = '1.0.0-beta';
