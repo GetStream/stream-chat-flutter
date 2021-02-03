2cd5eed2453dc4fcb69e4d47918b5f4c0a3a6303
Deven Joshi <deven9852@gmail.com>
2021-02-03 14:11:39 +0530

    fix: import

--- packages/stream_chat_flutter/lib/src/message_actions_modal.dart
+++ packages/stream_chat_flutter/lib/src/message_actions_modal.dart
@@ -8,7 +8,6 @@ import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 import 'package:stream_chat_flutter/src/reaction_picker.dart';
 import 'package:stream_chat_flutter/src/stream_svg_icon.dart';
 import 'package:stream_chat_flutter/src/utils.dart';
-import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
 
 import 'extension.dart';
 import 'message_input.dart';
