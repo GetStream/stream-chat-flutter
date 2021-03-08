9c9ef444b257012d663f9fbc855728a7150560b4
Sahil Kumar <xdsahil@gmail.com>
2021-03-08 15:07:13 +0530

    fix(UI-Kit): Make android example run on real devices
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

---
+++ packages/stream_chat_flutter/example/android/app/src/main/kotlin/com/example/example/Application.kt
@@ -0,0 +1,17 @@
+package com.example.example
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugins.pathprovider.PathProviderPlugin
+
+class Application : FlutterApplication(), PluginRegistrantCallback {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun registerWith(registry: PluginRegistry?) {
+        PathProviderPlugin.registerWith(registry?.registrarFor(
+                "io.flutter.plugins.pathprovider.PathProviderPlugin"))
+    }
+}
