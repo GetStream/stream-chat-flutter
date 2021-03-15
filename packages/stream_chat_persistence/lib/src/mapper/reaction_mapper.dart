8c0da62f792e68d0b08e1bff4f9bfd355130aef7
Sahil Kumar <xdsahil@gmail.com>
2021-03-15 12:05:36 +0530

    style: flutter format
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- packages/stream_chat_persistence/lib/src/mapper/reaction_mapper.dart
+++ packages/stream_chat_persistence/lib/src/mapper/reaction_mapper.dart
@@ -5,25 +5,25 @@ import 'package:stream_chat_persistence/src/db/moor_chat_database.dart';
 extension ReactionEntityX on ReactionEntity {
   /// Maps a [ReactionEntity] into [Reaction]
   Reaction toReaction({User user}) => Reaction(
-      extraData: extraData,
-      type: type,
-      createdAt: createdAt,
-      userId: userId,
-      user: user,
-      messageId: messageId,
-      score: score,
-    );
+        extraData: extraData,
+        type: type,
+        createdAt: createdAt,
+        userId: userId,
+        user: user,
+        messageId: messageId,
+        score: score,
+      );
 }
 
 /// Useful mapping functions for [Reaction]
 extension ReactionX on Reaction {
   /// Maps a [Reaction] into [ReactionEntity]
   ReactionEntity toEntity() => ReactionEntity(
-      extraData: extraData,
-      type: type,
-      createdAt: createdAt,
-      userId: userId,
-      messageId: messageId,
-      score: score,
-    );
+        extraData: extraData,
+        type: type,
+        createdAt: createdAt,
+        userId: userId,
+        messageId: messageId,
+        score: score,
+      );
 }
--- packages/stream_chat_persistence/lib/src/mapper/read_mapper.dart
+++ packages/stream_chat_persistence/lib/src/mapper/read_mapper.dart
@@ -5,19 +5,19 @@ import 'package:stream_chat_persistence/src/db/moor_chat_database.dart';
 extension ReadEntityX on ReadEntity {
   /// Maps a [ReadEntity] into [Read]
   Read toRead({User user}) => Read(
-      user: user,
-      lastRead: lastRead,
-      unreadMessages: unreadMessages,
-    );
+        user: user,
+        lastRead: lastRead,
+        unreadMessages: unreadMessages,
+      );
 }
 
 /// Useful mapping functions for [Read]
 extension ReadX on Read {
   /// Maps a [Read] into [ReadEntity]
   ReadEntity toEntity({String cid}) => ReadEntity(
-      lastRead: lastRead,
-      userId: user?.id,
-      channelCid: cid,
-      unreadMessages: unreadMessages,
-    );
+        lastRead: lastRead,
+        userId: user?.id,
+        channelCid: cid,
+        unreadMessages: unreadMessages,
+      );
 }
