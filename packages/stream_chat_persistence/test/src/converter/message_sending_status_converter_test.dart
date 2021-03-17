b738a47c2aa9949644105e9390ba3197b80424f7
Sahil Kumar <xdsahil@gmail.com>
2021-03-17 14:19:06 +0530

    test(persistence): add tests for message sending status converter
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

---
+++ packages/stream_chat_persistence/test/src/converter/message_sending_status_converter_test.dart
@@ -0,0 +1,33 @@
+import 'package:test/test.dart';
+import 'package:stream_chat/stream_chat.dart';
+import 'package:stream_chat_persistence/src/converter/message_sending_status_converter.dart';
+
+void main() {
+  group('mapToDart', () {
+    final statusConverter = MessageSendingStatusConverter();
+
+    test('should return null if nothing is provided', () {
+      final res = statusConverter.mapToDart(null);
+      expect(res, isNull);
+    });
+
+    test('should return expected status if status code is provided', () {
+      final res = statusConverter.mapToDart(3);
+      expect(res, MessageSendingStatus.updating);
+    });
+  });
+
+  group('mapToSql', () {
+    final statusConverter = MessageSendingStatusConverter();
+
+    test('should return null if nothing is provided', () {
+      final res = statusConverter.mapToSql(null);
+      expect(res, isNull);
+    });
+
+    test('should return expected code if the status is provided', () {
+      final res = statusConverter.mapToSql(MessageSendingStatus.updating);
+      expect(res, 3);
+    });
+  });
+}
