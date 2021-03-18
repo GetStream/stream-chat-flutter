e21d78fd7e39816294c687582bec3744f39fd559
Deven Joshi <deven9852@gmail.com>
2021-03-18 17:40:16 +0530

    feat: Added delete test

---
+++ packages/stream_chat_flutter/test/src/delete_message_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+
+import 'mocks.dart';
+
+void main() {
+  testWidgets(
+    'it should show basic channel information',
+    (WidgetTester tester) async {
+      final client = MockClient();
+      final clientState = MockClientState();
+
+      when(client.state).thenReturn(clientState);
+      when(clientState.user).thenReturn(OwnUser(id: 'user-id'));
+
+      await tester.pumpWidget(MaterialApp(
+        home: StreamChat(
+          client: client,
+          child: Scaffold(
+            body: DeletedMessage(
+              messageTheme: MessageTheme(
+                createdAt: TextStyle(
+                  color: Colors.black,
+                ),
+                messageText: TextStyle(),
+              ),
+            ),
+          ),
+        ),
+      ));
+
+      expect(find.text('Message deleted'), findsOneWidget);
+    },
+  );
+}
