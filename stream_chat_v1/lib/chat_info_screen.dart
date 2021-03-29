8cc7273f2814f1f72d2f8d1317fc78fb882b4070
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2021-03-29 12:38:36 +0200

    move file and media screens to sample app

--- stream_chat_v1/lib/chat_info_screen.dart
+++ stream_chat_v1/lib/chat_info_screen.dart
@@ -1,3 +1,5 @@
+import 'package:example/channel_file_display_screen.dart';
+import 'package:example/channel_media_display_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jiffy/jiffy.dart';
