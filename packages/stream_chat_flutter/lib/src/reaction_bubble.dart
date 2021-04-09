dd88cecedce433610fd19a7eea0adc95dcec63f5
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2021-04-09 11:38:25 +0200

    fix reaction bubble

--- packages/stream_chat_flutter/lib/src/reaction_bubble.dart
+++ packages/stream_chat_flutter/lib/src/reaction_bubble.dart
@@ -37,70 +37,68 @@ class ReactionBubble extends StatelessWidget {
     return Transform(
       transform: Matrix4.rotationY(reverse ? pi : 0),
       alignment: Alignment.center,
-      child: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          fit: StackFit.loose,
-          children: [
-            Transform.translate(
-              offset: Offset(reverse ? offset : -offset, 0),
+      child: Stack(
+        alignment: Alignment.center,
+        fit: StackFit.loose,
+        children: [
+          Transform.translate(
+            offset: Offset(reverse ? offset : -offset, 0),
+            child: Container(
+              padding: const EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                color: maskColor,
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+              ),
               child: Container(
-                padding: const EdgeInsets.all(2),
-                decoration: BoxDecoration(
-                  color: maskColor,
-                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                padding: EdgeInsets.symmetric(
+                  vertical: 4,
+                  horizontal: totalReactions > 1 ? 4 : 0,
                 ),
-                child: Container(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 4,
-                    horizontal: totalReactions > 1 ? 4 : 0,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: borderColor,
                   ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: borderColor,
-                    ),
-                    color: backgroundColor,
-                    borderRadius: BorderRadius.all(Radius.circular(14)),
-                  ),
-                  child: LayoutBuilder(
-                    builder: (context, constraints) {
-                      return Flex(
-                        direction: Axis.horizontal,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          if (constraints.maxWidth < double.infinity)
-                            ...reactions
-                                .take((constraints.maxWidth) ~/ 24)
-                                .map((reaction) {
-                              return _buildReaction(
-                                reactionIcons,
-                                reaction,
-                                context,
-                              );
-                            }).toList(),
-                          if (constraints.maxWidth == double.infinity)
-                            ...reactions.map((reaction) {
-                              return _buildReaction(
-                                reactionIcons,
-                                reaction,
-                                context,
-                              );
-                            }).toList(),
-                        ],
-                      );
-                    },
-                  ),
+                  color: backgroundColor,
+                  borderRadius: BorderRadius.all(Radius.circular(14)),
+                ),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    return Flex(
+                      direction: Axis.horizontal,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        if (constraints.maxWidth < double.infinity)
+                          ...reactions
+                              .take((constraints.maxWidth) ~/ 24)
+                              .map((reaction) {
+                            return _buildReaction(
+                              reactionIcons,
+                              reaction,
+                              context,
+                            );
+                          }).toList(),
+                        if (constraints.maxWidth == double.infinity)
+                          ...reactions.map((reaction) {
+                            return _buildReaction(
+                              reactionIcons,
+                              reaction,
+                              context,
+                            );
+                          }).toList(),
+                      ],
+                    );
+                  },
                 ),
               ),
             ),
-            Positioned(
-              bottom: 2,
-              left: reverse ? null : 13,
-              right: !reverse ? null : 13,
-              child: _buildReactionsTail(context),
-            ),
-          ],
-        ),
+          ),
+          Positioned(
+            bottom: 2,
+            left: reverse ? null : 13,
+            right: !reverse ? null : 13,
+            child: _buildReactionsTail(context),
+          ),
+        ],
       ),
     );
   }
