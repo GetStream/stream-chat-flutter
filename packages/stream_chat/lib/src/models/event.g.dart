7226d8c4d3691da153e550a45a0dd510dd7ccb90
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2021-04-20 09:35:11 +0200

    fix event

--- packages/stream_chat/lib/src/models/event.g.dart
+++ packages/stream_chat/lib/src/models/event.g.dart
@@ -39,7 +39,7 @@ Event _$EventFromJson(Map<String, dynamic> json) {
     channelType: json['channel_type'] as String?,
     parentId: json['parent_id'] as String?,
     extraData: json['extra_data'] as Map<String, dynamic>?,
-  )..isLocal = json['is_local'] as bool?;
+  )..isLocal = json['is_local'] as bool? ?? false;
 }
 
 Map<String, dynamic> _$EventToJson(Event instance) {
