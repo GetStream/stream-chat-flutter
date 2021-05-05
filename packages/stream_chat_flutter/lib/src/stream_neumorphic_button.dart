cca2e381034f2987bfd797663c298045f5f0976f
Deven Joshi <deven9852@gmail.com>
2021-05-05 17:23:38 +0530

    lint

--- packages/stream_chat_flutter/lib/src/stream_neumorphic_button.dart
+++ packages/stream_chat_flutter/lib/src/stream_neumorphic_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 /// Neumorphic button
 class StreamNeumorphicButton extends StatelessWidget {
-
   /// Constructor for creating [StreamNeumorphicButton]
   const StreamNeumorphicButton({
     Key? key,
@@ -18,24 +17,24 @@ class StreamNeumorphicButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Container(
-      margin: const EdgeInsets.all(8),
-      height: 40,
-      width: 40,
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        shape: BoxShape.circle,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.shade700,
-            offset: const Offset(0, 1),
-            blurRadius: 0.5,
-          ),
-          const BoxShadow(
-            color: Colors.white,
-            blurRadius: 0.5,
-          ),
-        ],
-      ),
-      child: child,
-    );
+        margin: const EdgeInsets.all(8),
+        height: 40,
+        width: 40,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade700,
+              offset: const Offset(0, 1),
+              blurRadius: 0.5,
+            ),
+            const BoxShadow(
+              color: Colors.white,
+              blurRadius: 0.5,
+            ),
+          ],
+        ),
+        child: child,
+      );
 }
