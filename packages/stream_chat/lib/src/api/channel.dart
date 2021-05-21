a543cf325a11fcfd8315ecbeaa211fecc37e1bef
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2021-05-21 10:50:35 +0200

    fix assert error message

--- packages/stream_chat/lib/src/api/channel.dart
+++ packages/stream_chat/lib/src/api/channel.dart
@@ -517,7 +517,7 @@ class Channel {
         throw ArgumentError('Invalid timeout or Expiration date');
       }
       return true;
-    }(), 'Check for invalid token or expiration date');
+    }(), 'Check whether time out is valid');
 
     DateTime? pinExpires;
     if (timeoutOrExpirationDate is DateTime) {
