dbb98811ab2e56df1949c69dea2e1a2e936449ab
Sahil Kumar <xdsahil@gmail.com>
2021-06-04 16:26:35 +0530

    push mocks.dart
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- packages/stream_chat/test/src/mocks.dart
+++ packages/stream_chat/test/src/mocks.dart
@@ -1,6 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:logging/logging.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:stream_chat/src/core/http/connection_id_manager.dart';
+import 'package:stream_chat/src/core/http/stream_http_client.dart';
+import 'package:stream_chat/src/core/http/token_manager.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class MockWebSocketChannel extends Mock implements WebSocketChannel {}
 
 class MockWebSocketSink extends Mock implements WebSocketSink {}
+
+class MockDio extends Mock implements Dio {
+  BaseOptions? _options;
+
+  @override
+  BaseOptions get options => _options ??= BaseOptions();
+
+  Interceptors? _interceptors;
+
+  @override
+  Interceptors get interceptors => _interceptors ??= Interceptors();
+}
+
+class MockLogger extends Mock implements Logger {
+  @override
+  Level get level => Level.ALL;
+}
+
+class MockHttpClient extends Mock implements StreamHttpClient {}
+
+class MockTokenManager extends Mock implements TokenManager {}
+
+class MockConnectionIdManager extends Mock implements ConnectionIdManager {}
