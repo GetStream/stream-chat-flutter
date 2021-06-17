91907639adef5627dc9f4f4fefd7f604978b4f5e
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2021-06-17 10:01:46 +0200

    add more user and channel config test

--- packages/stream_chat/test/src/core/models/user_test.dart
+++ packages/stream_chat/test/src/core/models/user_test.dart
@@ -8,6 +8,7 @@ void main() {
     test('should parse json correctly', () {
       final user = User.fromJson(jsonFixture('user.json'));
       expect(user.id, 'bbb19d9a-ee50-45bc-84e5-0584e79d0c9e');
+      expect(user.name, 'John');
     });
 
     test('should serialize to json correctly', () {
@@ -19,6 +20,27 @@ void main() {
       expect(user.toJson(), {
         'id': 'bbb19d9a-ee50-45bc-84e5-0584e79d0c9e',
       });
+    });
+
+    test('copyWith', () {
+      final user = User.fromJson(jsonFixture('user.json'));
+      var newUser = user.copyWith();
+
+      expect(newUser.id, user.id);
+      expect(newUser.role, user.role);
+      expect(newUser.name, user.name);
+
+      newUser = user.copyWith(
+        id: 'test',
+        role: 'test',
+        extraData: {
+          'name': 'test',
+        },
+      );
+
+      expect(newUser.id, 'test');
+      expect(newUser.role, 'test');
+      expect(newUser.name, 'test');
     });
   });
 }
