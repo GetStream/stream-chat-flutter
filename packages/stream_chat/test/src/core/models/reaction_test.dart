6a2efb18e44f9447e4781178fa8ada09ca2fe148
Sahil Kumar <xdsahil@gmail.com>
2021-06-17 15:01:45 +0530

    Update packages/stream_chat/test/src/core/models/reaction_test.dart

--- packages/stream_chat/test/src/core/models/reaction_test.dart
+++ packages/stream_chat/test/src/core/models/reaction_test.dart
@@ -77,34 +77,16 @@ void main() {
         userId: 'test',
       );
 
-      expect(
-        newReaction.type,
-        'lol',
-      );
+      expect(newReaction.type, 'lol');
       expect(
         newReaction.createdAt,
         DateTime.parse('2021-01-28T22:17:31.108742Z'),
       );
-      expect(
-        newReaction.extraData,
-        {},
-      );
-      expect(
-        newReaction.messageId,
-        'test',
-      );
-      expect(
-        newReaction.score,
-        2,
-      );
-      expect(
-        newReaction.user,
-        User(id: 'test'),
-      );
-      expect(
-        newReaction.userId,
-        'test',
-      );
+      expect(newReaction.extraData, {});
+      expect(newReaction.messageId, 'test');
+      expect(newReaction.score, 2);
+      expect(newReaction.user, User(id: 'test'));
+      expect(newReaction.userId, 'test');
     });
 
     test('merge', () {
