c7d77c8cd93798f20b0600e0c1adfa72c2918f43
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2021-06-21 15:29:45 +0200

    fix gh action

--- .github/workflows/scripts/remove-from-coverage.sh
+++ .github/workflows/scripts/remove-from-coverage.sh
@@ -3,5 +3,4 @@
 # Fast fail the script on failures.
 set -e
 
-pub global activate remove_from_coverage
 pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '\.g\.dart$' -r '\.freezed\.dart$'
