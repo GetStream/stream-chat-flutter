57b6382038b26f914579acc58a54652f4f94c708
groovinchip <groovinchip@gmail.com>
2021-07-14 10:46:58 -0400

    test: add dark theme default values test

--- packages/stream_chat_flutter/test/src/image_header_theme_test.dart
+++ packages/stream_chat_flutter/test/src/image_header_theme_test.dart
@@ -50,6 +50,45 @@ void main() {
     expect(imageHeaderTheme.bottomSheetBarrierColor,
         _imageHeaderThemeDataControl.bottomSheetBarrierColor);
   });
+
+  testWidgets(
+      'Passing no ImageHeaderThemeData returns default dark theme values',
+          (WidgetTester tester) async {
+        late BuildContext _context;
+        await tester.pumpWidget(
+          MaterialApp(
+            builder: (context, child) => StreamChat(
+              client: MockStreamChatClient(),
+              streamChatThemeData: StreamChatThemeData.dark(),
+              child: child,
+            ),
+            home: Builder(
+              builder: (context) {
+                _context = context;
+                return Scaffold(
+                  appBar: ImageHeader(
+                    message: Message(),
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+
+        final imageHeaderTheme = ImageHeaderTheme.of(_context);
+        expect(imageHeaderTheme.closeButtonColor,
+            _imageHeaderThemeDataDarkControl.closeButtonColor);
+        expect(imageHeaderTheme.backgroundColor,
+            _imageHeaderThemeDataDarkControl.backgroundColor);
+        expect(imageHeaderTheme.iconMenuPointColor,
+            _imageHeaderThemeDataDarkControl.iconMenuPointColor);
+        expect(imageHeaderTheme.titleTextStyle,
+            _imageHeaderThemeDataDarkControl.titleTextStyle);
+        expect(imageHeaderTheme.subtitleTextStyle,
+            _imageHeaderThemeDataDarkControl.subtitleTextStyle);
+        expect(imageHeaderTheme.bottomSheetBarrierColor,
+            _imageHeaderThemeDataDarkControl.bottomSheetBarrierColor);
+      });
 }
 
 // Light theme test control.
@@ -71,4 +110,25 @@ final _imageHeaderThemeDataControl = ImageHeaderThemeData(
     color: const Color(0xff7A7A7A),
   ),
   bottomSheetBarrierColor: const Color.fromRGBO(0, 0, 0, 0.2),
+);
+
+// Light theme test control.
+//
+// Test default ImageHeaderThemeData values against this control.
+final _imageHeaderThemeDataDarkControl = ImageHeaderThemeData(
+  closeButtonColor: const Color(0xffffffff),
+  backgroundColor: const Color(0xff101418),
+  iconMenuPointColor: const Color(0xffffffff),
+  titleTextStyle: const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  ),
+  subtitleTextStyle: const TextStyle(
+    fontSize: 12,
+    color: Colors.white,
+  ).copyWith(
+    color: const Color(0xff7A7A7A),
+  ),
+  bottomSheetBarrierColor: const Color.fromRGBO(0, 0, 0, 0.4),
 );
