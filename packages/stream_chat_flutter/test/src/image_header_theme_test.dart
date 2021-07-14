6354ce93ebc1eac555e297dd618e2b144102de66
groovinchip <groovinchip@gmail.com>
2021-07-13 21:02:34 -0400

    test: add tests for default ImageHeaderThemeData values
    Tests the default values of ImageHeaderThemeData that gets built in a StreamChatThemeData against control values.

--- packages/stream_chat_flutter/test/src/image_header_theme_test.dart
+++ packages/stream_chat_flutter/test/src/image_header_theme_test.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockStreamChatClient extends Mock implements StreamChatClient {}
 
 void main() {
   test('ImageHeaderThemeData copyWith, ==, hashCode basics', () {
@@ -8,4 +12,59 @@ void main() {
     expect(const ImageHeaderThemeData().hashCode,
         const ImageHeaderThemeData().copyWith().hashCode);
   });
+
+  testWidgets('Passing no ImageHeaderThemeData returns defaults',
+      (WidgetTester tester) async {
+    late BuildContext _context;
+    await tester.pumpWidget(
+      MaterialApp(
+        builder: (context, child) => StreamChat(
+          client: MockStreamChatClient(),
+          child: child,
+        ),
+        home: Builder(
+          builder: (context) {
+            _context = context;
+            return Scaffold(
+              appBar: ImageHeader(
+                message: Message(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+
+    final imageHeaderTheme = StreamChatTheme.of(_context).imageHeaderTheme;
+    expect(imageHeaderTheme.closeButtonColor,
+        _imageHeaderThemeDataControl.closeButtonColor);
+    expect(imageHeaderTheme.backgroundColor,
+        _imageHeaderThemeDataControl.backgroundColor);
+    expect(imageHeaderTheme.iconMenuPointColor,
+        _imageHeaderThemeDataControl.iconMenuPointColor);
+    expect(imageHeaderTheme.titleTextStyle,
+        _imageHeaderThemeDataControl.titleTextStyle);
+    expect(imageHeaderTheme.subtitleTextStyle,
+        _imageHeaderThemeDataControl.subtitleTextStyle);
+    expect(imageHeaderTheme.bottomSheetBarrierColor,
+        _imageHeaderThemeDataControl.bottomSheetBarrierColor);
+  });
 }
+
+final _imageHeaderThemeDataControl = ImageHeaderThemeData(
+  closeButtonColor: const Color(0xff000000),
+  backgroundColor: const Color(0xffffffff),
+  iconMenuPointColor: const Color(0xff000000),
+  titleTextStyle: const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  ),
+  subtitleTextStyle: const TextStyle(
+    fontSize: 12,
+    color: Colors.black,
+  ).copyWith(
+    color: const Color(0xff7A7A7A),
+  ),
+  bottomSheetBarrierColor: const Color.fromRGBO(0, 0, 0, 0.2),
+);
