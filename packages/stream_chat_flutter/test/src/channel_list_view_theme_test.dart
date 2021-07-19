f904b3ed829e3cc459ab1a3315e48851ed440a0e
groovinchip <groovinchip@gmail.com>
2021-07-19 09:05:50 -0400

    remove unused import

--- packages/stream_chat_flutter/test/src/channel_list_view_theme_test.dart
+++ packages/stream_chat_flutter/test/src/channel_list_view_theme_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+
 import 'mocks.dart';
 
 void main() {
