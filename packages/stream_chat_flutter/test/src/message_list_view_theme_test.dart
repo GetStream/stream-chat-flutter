10d2480a00e02b3a51b895daba3977cc113966ca
groovinchip <groovinchip@gmail.com>
2021-07-19 14:41:39 -0400

    test: add tests for MessageSearchListViewThemeData

--- packages/stream_chat_flutter/test/src/message_list_view_theme_test.dart
+++ packages/stream_chat_flutter/test/src/message_list_view_theme_test.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
-import 'package:flutter_test/flutter_test.dart';
+
 import 'mocks.dart';
 
 class MockStreamChatClient extends Mock implements StreamChatClient {}
