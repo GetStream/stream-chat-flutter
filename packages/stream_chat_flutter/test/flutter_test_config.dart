526450d4de5eb0d59dec8f24c84c5ccd079db697
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2021-07-22 14:48:42 +0200

    use custom golden comparator

--- packages/stream_chat_flutter/test/flutter_test_config.dart
+++ packages/stream_chat_flutter/test/flutter_test_config.dart
@@ -1,9 +1,36 @@
 import 'dart:async';
+import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   await loadAppFonts();
+  goldenFileComparator =
+      CustomGoldenFileComparator(Uri.parse('test/src/goldens'));
   return testMain();
+}
+
+class CustomGoldenFileComparator extends LocalFileComparator {
+  CustomGoldenFileComparator(Uri testFile) : super(testFile);
+
+  @override
+  Future<bool> compare(Uint8List imageBytes, Uri golden) async {
+    final result = await GoldenFileComparator.compareLists(
+      imageBytes,
+      await getGoldenBytes(golden),
+    );
+
+    if (!result.passed && result.diffPercent > 0.05) {
+      final error = await generateFailureOutput(result, golden, basedir);
+      throw FlutterError(error);
+    }
+    return result.passed;
+  }
+
+  @override
+  Future<void> update(Uri golden, Uint8List imageBytes) {
+    return super.update(golden, imageBytes);
+  }
 }
