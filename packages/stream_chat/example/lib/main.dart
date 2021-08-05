3ab363a8b297a58351c20905d50caec7533faabf
Sahil Kumar <xdsahil@gmail.com>
2021-08-05 16:38:32 +0530

    chore(llc, ui, core, localization, persistence): analysis fixes, move `extraData.image` to `image` field
    Signed-off-by: xsahil03x <xdsahil@gmail.com>

--- packages/stream_chat/example/lib/main.dart
+++ packages/stream_chat/example/lib/main.dart
@@ -13,10 +13,9 @@ Future<void> main() async {
   await client.connectUser(
     User(
       id: 'cool-shadow-7',
-      extraData: const {
-        'image':
-            'https://getstream.io/random_png/?id=cool-shadow-7&amp;name=Cool+shadow',
-      },
+      name: 'Cool Shadow',
+      image:
+          'https://getstream.io/random_png/?id=cool-shadow-7&amp;name=Cool+shadow',
     ),
     '''eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiY29vbC1zaGFkb3ctNyJ9.gkOlCRb1qgy4joHPaxFwPOdXcGvSPvp6QY0S4mpRkVo''',
   );
--- packages/stream_chat_persistence/example/lib/main.dart
+++ packages/stream_chat_persistence/example/lib/main.dart
@@ -22,10 +22,8 @@ Future<void> main() async {
   await client.connectUser(
     User(
       id: 'cool-shadow-7',
-      extraData: const {
-        'image':
-            'https://getstream.io/random_png/?id=cool-shadow-7&amp;name=Cool+shadow',
-      },
+      image:
+          'https://getstream.io/random_png/?id=cool-shadow-7&amp;name=Cool+shadow',
     ),
     'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiY29vbC1zaGFkb3ctNyJ9.'
     'gkOlCRb1qgy4joHPaxFwPOdXcGvSPvp6QY0S4mpRkVo',
