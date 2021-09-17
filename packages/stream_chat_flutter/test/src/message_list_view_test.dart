7d362db390d00bde1b0f2a1af4e998bda218a2f8
Deven Joshi <deven9852@gmail.com>
2021-09-17 18:34:00 +0530

    fix(ui): check scrollController is attached before calling jump.

---
+++ packages/stream_chat_flutter/test/src/message_list_view_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+
+import 'mocks.dart';
+
+void main() {
+  late StreamChatClient client;
+  late Channel channel;
+
+  setUp(() {
+    client = MockClient();
+    channel = MockChannel();
+    when(() => channel.on(any(), any(), any(), any()))
+        .thenAnswer((_) => const Stream.empty());
+    final channelClientState = MockChannelState();
+    when(() => channel.state).thenReturn(channelClientState);
+    when(() => channelClientState.threadsStream)
+        .thenAnswer((_) => const Stream.empty());
+    when(() => channelClientState.messagesStream)
+        .thenAnswer((_) => const Stream.empty());
+    when(() => channelClientState.messages).thenReturn([]);
+    when(() => channelClientState.isUpToDate).thenReturn(true);
+  });
+
+  // https://github.com/GetStream/stream-chat-flutter/issues/674
+  testWidgets('renders empty message list view', (tester) async {
+    const emptyWidgetKey = Key('empty_widget');
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: StreamChat(
+          client: client,
+          child: StreamChannel(
+            channel: channel,
+            child: MessageListView(
+              emptyBuilder: (_) => Container(key: emptyWidgetKey),
+            ),
+          ),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.byType(MessageListView), findsOneWidget);
+    expect(find.byKey(emptyWidgetKey), findsOneWidget);
+  });
+}
