bf30102d2b4a676edfe8bab7436112a6b78f1df0
Deven Joshi <deven9852@gmail.com>
2021-11-03 14:11:56 +0530

    analysis fixes

--- packages/stream_chat_flutter/lib/src/mip/countdown_button.dart
+++ packages/stream_chat_flutter/lib/src/mip/countdown_button.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
+/// Button for showing visual component of slow mode.
 class CountdownButton extends StatelessWidget {
+
+  /// Constructor for creating [CountdownButton].
   const CountdownButton({
     Key? key,
     required this.count,
   }) : super(key: key);
 
+  /// Count of time remaining to show to the user.
   final int count;
 
   @override
