687fa6cfce4494416a201170526349c8a64e0c59
Deven Joshi <deven9852@gmail.com>
2021-11-15 19:13:07 +0530

    added back dio

--- packages/stream_chat_flutter/lib/src/gallery_footer.dart
+++ packages/stream_chat_flutter/lib/src/gallery_footer.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
