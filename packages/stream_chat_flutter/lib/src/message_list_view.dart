5c018cdbbd031d11c59565faaa0e6c1034097db5
Fabian Bardecio <fbardecio@somniosoftware.com>
2022-01-05 20:47:37 -0300

    changed chat styling

--- packages/stream_chat_flutter/lib/src/message_list_view.dart
+++ packages/stream_chat_flutter/lib/src/message_list_view.dart
@@ -341,6 +341,9 @@ class MessageListView extends StatefulWidget {
   /// message, default spacing, etc)
   final SpacingWidgetBuilder? spacingWidgetBuilder;
 
+  ///If true shows user name at the top of the message next to the user avatar
+  final bool showUserNameAtTop = false;
+
   @override
   _MessageListViewState createState() => _MessageListViewState();
 }
@@ -994,7 +997,8 @@ class _MessageListViewState extends State<MessageListView> {
       showEditMessage: false,
       message: message,
       reverse: isMyMessage,
-      showUsername: !isMyMessage,
+      showUserNameAtTop: widget.showUserNameAtTop,
+      showUserNameAtBottom: !isMyMessage && !widget.showUserNameAtTop,
       padding: const EdgeInsets.all(8),
       showSendingIndicator: false,
       borderRadiusGeometry: BorderRadius.only(
@@ -1088,7 +1092,7 @@ class _MessageListViewState extends State<MessageListView> {
         !hasReplies &&
         (timeDiff >= 1 || !isNextUserSame);
 
-    final showUsername = !isMyMessage &&
+    final showUserNameAtBottom = !isMyMessage &&
         (!isThreadMessage || _isThreadConversation) &&
         !hasReplies &&
         (timeDiff >= 1 || !isNextUserSame);
@@ -1126,7 +1130,7 @@ class _MessageListViewState extends State<MessageListView> {
       padding: const EdgeInsets.symmetric(horizontal: 8),
       showInChannelIndicator: showInChannelIndicator,
       showThreadReplyIndicator: showThreadReplyIndicator,
-      showUsername: showUsername,
+      showUserNameAtBottom: showUserNameAtBottom,
       showTimestamp: showTimeStamp,
       showSendingIndicator: showSendingIndicator,
       showUserAvatar: showUserAvatar,
