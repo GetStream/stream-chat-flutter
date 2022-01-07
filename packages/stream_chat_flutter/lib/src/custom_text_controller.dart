40522098497aaaca49ca368f62f402eb8a3445a8
Fabian Bardecio <fbardecio@somniosoftware.com>
2022-01-07 17:57:04 -0300

    Added custom controller to render styled text in input

--- packages/stream_chat_flutter/lib/src/custom_text_controller.dart
+++ packages/stream_chat_flutter/lib/src/custom_text_controller.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
+///Custom text controller that handle different text styles when a Roger
+///user is mentioned in the input field.
 class CustomTextController extends TextEditingController {
+  ///Set of Roger available users.
   final Set<User> users = {};
 
+  ///Method used to add Roger available users to the text controller.
   void addUsers(Set<User> newUsers) {
     users.addAll(newUsers);
   }
@@ -15,7 +19,6 @@ class CustomTextController extends TextEditingController {
     required bool withComposing,
   }) =>
       TextSpan(
-        text: text,
         children: _mentionBuilder(text, users),
         style: const TextStyle(color: Colors.black),
       );
@@ -23,31 +26,56 @@ class CustomTextController extends TextEditingController {
 
 List<InlineSpan> _mentionBuilder(String text, Set<User> users) {
   final styledWords = <TextSpan>[];
-  var editedText = '';
+  final namesAsMentions = <String>[];
+  var editedText = text;
 
   if (text.isNotEmpty) {
     for (final user in users) {
       final nameAsMention = '@${user.name}';
-      editedText = text.replaceAll(nameAsMention, '|||@${user.name}|||');
+      namesAsMentions.add(nameAsMention);
+      editedText = editedText.replaceAll(nameAsMention, '|||@${user.name}|||');
     }
-
     final splittedMessage = editedText.split('|||');
-    for (final sentence in splittedMessage) {
-      if (sentence.contains('@')) {
-        styledWords.add(
-          TextSpan(
-            text: sentence,
-            style: const TextStyle(color: Colors.lightBlue),
-          ),
-        );
-      } else {
-        styledWords.add(_normalTextSpan(sentence));
-      }
-    }
+
+    styledWords.addAll(
+      _mentionTextSpan(
+        splittedMessage: splittedMessage,
+        names: namesAsMentions,
+      ),
+    );
   } else {
     styledWords.add(_normalTextSpan(text));
   }
   return styledWords;
+}
+
+List<TextSpan> _mentionTextSpan({
+  required List<String> splittedMessage,
+  required List<String> names,
+}) {
+  //ignore: prefer_final_locals
+  var result = <TextSpan>[];
+  for (final sentence in splittedMessage) {
+    var isMention = false;
+    names.forEach((name) {
+      if (sentence.contains(name)) {
+        isMention = true;
+      }
+    });
+
+    if (isMention) {
+      result.add(
+        TextSpan(
+          text: sentence,
+          style: const TextStyle(color: Colors.lightBlue),
+        ),
+      );
+    } else {
+      result.add(_normalTextSpan(sentence));
+    }
+  }
+
+  return result;
 }
 
 TextSpan _normalTextSpan(String sentence) => TextSpan(
