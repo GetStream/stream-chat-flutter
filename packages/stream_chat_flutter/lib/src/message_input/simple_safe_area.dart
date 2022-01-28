9edf02588ddfc4a8b27f149ce1c3385279164b0c
Deven Joshi <deven9852@gmail.com>
2022-01-28 16:39:57 +0530

    docs

--- packages/stream_chat_flutter/lib/src/message_input/simple_safe_area.dart
+++ packages/stream_chat_flutter/lib/src/message_input/simple_safe_area.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
+/// A [SafeArea] with an enabled toggle
 class SimpleSafeArea extends StatefulWidget {
-  final bool enabled;
-  final Widget child;
-
+  /// Constructor for [SimpleSafeArea]
   const SimpleSafeArea({
     Key? key,
     this.enabled = true,
     required this.child,
   }) : super(key: key);
+
+  final bool enabled;
+  final Widget child;
 
   @override
   _SimpleSafeAreaState createState() => _SimpleSafeAreaState();
