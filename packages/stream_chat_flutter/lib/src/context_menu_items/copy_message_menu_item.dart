7c98217eab1e2acc917e9fa9487558262b9c5a47
GroovinChip <groovinchip@gmail.com>
2022-02-22 16:50:04 -0500

    feat(flutter): Copy Message menu item

---
+++ packages/stream_chat_flutter/lib/src/context_menu_items/copy_message_menu_item.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:native_context_menu/native_context_menu.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+
+/// A [MenuItem] that will allow a user to copy the text of a message.
+///
+/// Used only for desktop and web platforms.
+class CopyMessageMenuItem extends MenuItem {
+  /// Builds a [CopyMessageMenuItem].
+  CopyMessageMenuItem({
+    String title = 'Copy Message',
+    required this.message,
+  }) : super(title: title);
+
+  /// The message to copy.
+  final Message message;
+
+  @override
+  VoidCallback? get onSelected =>
+      () => Clipboard.setData(ClipboardData(text: message.text));
+}
