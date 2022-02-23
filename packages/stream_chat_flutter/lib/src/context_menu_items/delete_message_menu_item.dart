8294ed49682e382b02342a9477f98b07bdabff43
GroovinChip <groovinchip@gmail.com>
2022-02-23 09:48:17 -0500

    feat(flutter): basic delete message menu item
    The logic for actually deleting the message will be implemented later.

---
+++ packages/stream_chat_flutter/lib/src/context_menu_items/delete_message_menu_item.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:native_context_menu/native_context_menu.dart';
+
+/// A [MenuItem] that will allow a user to delete a message.
+///
+/// Used only for desktop and web platforms.
+class DeleteMessageMenuItem extends MenuItem {
+  /// Builds a [DeleteMessageMenuItem].
+  DeleteMessageMenuItem({
+    String title = 'Delete Message',
+    required this.onClick,
+  }) : super(title: title);
+
+  /// The action to perform when the menu item is clicked.
+  ///
+  /// This is used to keep Flutter-specific code out of the API for
+  /// this class. Subject to change.
+  final VoidCallback onClick;
+
+  @override
+  VoidCallback? get onSelected => onClick.call;
+}
