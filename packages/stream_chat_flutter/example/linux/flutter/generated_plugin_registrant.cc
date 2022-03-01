ac327586bcebf38bbb34a1f5bf3d572e85eb5d27
GroovinChip <groovinchip@gmail.com>
2022-03-01 10:19:00 -0500

    feat(flutter): add Lin ux runner to example app

---
+++ packages/stream_chat_flutter/example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,23 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <file_selector_linux/file_selector_plugin.h>
+#include <native_context_menu/native_context_menu_plugin.h>
+#include <url_launcher_linux/url_launcher_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) file_selector_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FileSelectorPlugin");
+  file_selector_plugin_register_with_registrar(file_selector_linux_registrar);
+  g_autoptr(FlPluginRegistrar) native_context_menu_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "NativeContextMenuPlugin");
+  native_context_menu_plugin_register_with_registrar(native_context_menu_registrar);
+  g_autoptr(FlPluginRegistrar) url_launcher_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "UrlLauncherPlugin");
+  url_launcher_plugin_register_with_registrar(url_launcher_linux_registrar);
+}
