7eecee5cded4252033ff77d17923867687094efd
GroovinChip <groovinchip@gmail.com>
2022-03-03 10:24:35 -0500

    chore(flutter): put PickerWidget in its own file

---
+++ packages/stream_chat_flutter/lib/src/message_input/picker_widget.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:photo_manager/photo_manager.dart';
+import 'package:stream_chat_flutter/src/extension.dart';
+import 'package:stream_chat_flutter/src/media_list_view.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+
+/// Allows user to select files to upload to a chat.
+///
+/// Should only be used on mobile - breaks on desktop & web due to the
+/// permissions plugin not being supported.
+class PickerWidget extends StatefulWidget {
+  /// Builds a [PickerWidget].
+  const PickerWidget({
+    Key? key,
+    required this.filePickerIndex,
+    required this.containsFile,
+    required this.selectedMedias,
+    required this.onAddMoreFilesClick,
+    required this.onMediaSelected,
+    required this.streamChatTheme,
+  }) : super(key: key);
+
+  ///
+  final int filePickerIndex;
+
+  ///
+  final bool containsFile;
+
+  /// The selected media to upload.
+  final List<String> selectedMedias;
+
+  /// The callback to perform when adding more files.
+  final void Function(DefaultAttachmentTypes) onAddMoreFilesClick;
+
+  /// The callback to perform when a media is selected.
+  final void Function(AssetEntity) onMediaSelected;
+
+  /// The theme to use for this widget.
+  final StreamChatThemeData streamChatTheme;
+
+  @override
+  _PickerWidgetState createState() => _PickerWidgetState();
+}
+
+class _PickerWidgetState extends State<PickerWidget> {
+  Future<bool>? requestPermission;
+
+  @override
+  void initState() {
+    super.initState();
+    requestPermission = PhotoManager.requestPermission();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.filePickerIndex != 0) {
+      return const Offstage();
+    }
+    return FutureBuilder<bool>(
+      future: requestPermission,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Offstage();
+        }
+
+        if (snapshot.data!) {
+          if (widget.containsFile) {
+            return GestureDetector(
+              onTap: () {
+                widget.onAddMoreFilesClick(DefaultAttachmentTypes.file);
+              },
+              child: Container(
+                constraints: const BoxConstraints.expand(),
+                color: widget.streamChatTheme.colorTheme.inputBg,
+                alignment: Alignment.center,
+                child: Text(
+                  context.translations.addMoreFilesLabel,
+                  style: TextStyle(
+                    color: widget.streamChatTheme.colorTheme.accentPrimary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            );
+          }
+          return MediaListView(
+            selectedIds: widget.selectedMedias,
+            onSelect: widget.onMediaSelected,
+          );
+        }
+
+        return InkWell(
+          onTap: () async => PhotoManager.openSetting(),
+          child: Container(
+            color: widget.streamChatTheme.colorTheme.inputBg,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SvgPicture.asset(
+                  'svgs/icon_picture_empty_state.svg',
+                  package: 'stream_chat_flutter',
+                  height: 140,
+                  color: widget.streamChatTheme.colorTheme.disabled,
+                ),
+                Text(
+                  context.translations.enablePhotoAndVideoAccessMessage,
+                  style: widget.streamChatTheme.textTheme.body.copyWith(
+                    color: widget.streamChatTheme.colorTheme.textLowEmphasis,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 6),
+                Center(
+                  child: Text(
+                    context.translations.allowGalleryAccessMessage,
+                    style: widget.streamChatTheme.textTheme.bodyBold.copyWith(
+                      color: widget.streamChatTheme.colorTheme.accentPrimary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
