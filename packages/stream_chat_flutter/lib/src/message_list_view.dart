b11a812721d6d0e8508afd2ed8a32a3f32c5099b
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2022-03-24 11:42:46 +0100

    fix(ui): fix initial alignment in message list view

--- packages/stream_chat_flutter/lib/src/message_list_view.dart
+++ packages/stream_chat_flutter/lib/src/message_list_view.dart
@@ -369,7 +369,7 @@ class _MessageListViewState extends State<MessageListView> {
   double get _initialAlignment {
     final initialAlignment = widget.initialAlignment;
     if (initialAlignment != null) return initialAlignment;
-    return 0.1;
+    return streamChannel!.initialMessageId == null ? 0 : 0.1;
   }
 
   bool _isInitialMessage(String id) => streamChannel!.initialMessageId == id;
