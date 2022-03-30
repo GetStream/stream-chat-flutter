679faffbaf219b7e22e357e7ce56bd6ab898783c
Sahil Kumar <xdsahil@gmail.com>
2022-03-30 20:43:21 +0530

    fix(llc): segregate mute from channel mutes
    Signed-off-by: xsahil03x <xdsahil@gmail.com>

--- packages/stream_chat/lib/src/core/models/channel_mute.dart
+++ packages/stream_chat/lib/src/core/models/channel_mute.dart
@@ -1,28 +1,30 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:stream_chat/src/core/models/channel_model.dart';
 import 'package:stream_chat/src/core/models/user.dart';
 
-part 'mute.g.dart';
+part 'channel_mute.g.dart';
 
 /// The class that contains the information about a muted user
 @JsonSerializable(createToJson: false)
-class Mute {
+class ChannelMute {
   /// Constructor used for json serialization
-  Mute({
+  ChannelMute({
     required this.user,
-    required this.target,
+    required this.channel,
     required this.createdAt,
     required this.updatedAt,
     this.expires,
   });
 
   /// Create a new instance from a json
-  factory Mute.fromJson(Map<String, dynamic> json) => _$MuteFromJson(json);
+  factory ChannelMute.fromJson(Map<String, dynamic> json) =>
+      _$ChannelMuteFromJson(json);
 
   /// The user that performed the muting action
   final User user;
 
   /// The target user
-  final User target;
+  final ChannelModel channel;
 
   /// The date in which the use was muted
   final DateTime createdAt;
--- packages/stream_chat/test/src/core/models/channel_mute_test.dart
+++ packages/stream_chat/test/src/core/models/channel_mute_test.dart
@@ -1,14 +1,14 @@
 import 'package:stream_chat/src/core/models/channel_model.dart';
-import 'package:stream_chat/src/core/models/mute.dart';
+import 'package:stream_chat/src/core/models/channel_mute.dart';
 import 'package:stream_chat/src/core/models/user.dart';
 import 'package:test/test.dart';
 
 import '../../utils.dart';
 
 void main() {
-  group('src/models/mute', () {
+  group('src/models/channel_mute', () {
     test('should parse json correctly', () {
-      final mute = Mute.fromJson(jsonFixture('mute.json'));
+      final mute = ChannelMute.fromJson(jsonFixture('channel_mute.json'));
       expect(mute.channel, isA<ChannelModel>());
       expect(mute.user, isA<User>());
       expect(mute.createdAt, DateTime.parse('2020-12-04T10:39:06.512021Z'));
