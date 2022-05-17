45b27c4fe53b116787b045dd148b15b7494887e3
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2022-05-17 11:03:18 +0200

    fix android with flutter 3.0

--- stream_chat_v1/android/app/src/main/kotlin/com/example/example/Application.kt
+++ stream_chat_v1/android/app/src/main/kotlin/com/example/example/Application.kt
@@ -12,12 +12,12 @@ class Application : FlutterApplication(), PluginRegistrantCallback {
         super.onCreate()
     }
 
-    override fun registerWith(registry: PluginRegistry?) {
-        PathProviderPlugin.registerWith(registry?.registrarFor(
+    override fun registerWith(registry: PluginRegistry) {
+        PathProviderPlugin.registerWith(registry.registrarFor(
                 "io.flutter.plugins.pathprovider.PathProviderPlugin"))
-        SharedPreferencesPlugin.registerWith(registry?.registrarFor(
+        SharedPreferencesPlugin.registerWith(registry.registrarFor(
                 "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin"))
-        FlutterLocalNotificationsPlugin.registerWith(registry?.registrarFor(
+        FlutterLocalNotificationsPlugin.registerWith(registry.registrarFor(
                 "com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin"))
     }
 }
