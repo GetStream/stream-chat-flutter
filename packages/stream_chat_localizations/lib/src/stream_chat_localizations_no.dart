16028a4433a70fee437b2c6a0b67f222b2a4592c
Sahil Kumar <xdsahil@gmail.com>
2022-07-05 15:36:58 +0530

    Apply suggestions from code review

--- packages/stream_chat_localizations/lib/src/stream_chat_localizations_no.dart
+++ packages/stream_chat_localizations/lib/src/stream_chat_localizations_no.dart
@@ -1,9 +1,9 @@
 part of 'stream_chat_localizations.dart';
 
-/// The translations for English (`en`).
-class StreamChatLocalizationsEn extends GlobalStreamChatLocalizations {
+/// The translations for Norwegian (`no`).
+class StreamChatLocalizationsNo extends GlobalStreamChatLocalizations {
   /// Create an instance of the translation bundle for English.
-  const StreamChatLocalizationsEn({super.localeName = 'no'});
+  const StreamChatLocalizationsNo({super.localeName = 'no'});
 
   @override
   String get launchUrlError => 'Kan ikke laste inn url';
