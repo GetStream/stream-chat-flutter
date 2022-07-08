265771aa0ec89c987706fa54cd4932c94bfb335b
Sahil Kumar <xdsahil@gmail.com>
2022-07-08 19:06:12 +0530

    chore(ui): minor changes
    Signed-off-by: xsahil03x <xdsahil@gmail.com>

--- packages/stream_chat_flutter/lib/src/message_list_view.dart
+++ packages/stream_chat_flutter/lib/src/message_list_view.dart
@@ -206,7 +206,7 @@ class StreamMessageListView extends StatefulWidget {
     this.paginationLimit = 20,
     this.paginationLoadingIndicatorBuilder,
     this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.onDrag,
-    this.spacingWidgetBuilder,
+    this.spacingWidgetBuilder = _defaultSpacingWidgetBuilder,
   });
 
   /// [ScrollViewKeyboardDismissBehavior] the defines how this [PositionedList] will
@@ -353,7 +353,18 @@ class StreamMessageListView extends StatefulWidget {
   /// A List of [SpacingType] is provided to provide more data about the
   /// type of message (thread, difference in time between current and last
   /// message, default spacing, etc)
-  final SpacingWidgetBuilder? spacingWidgetBuilder;
+  final SpacingWidgetBuilder spacingWidgetBuilder;
+
+  static Widget _defaultSpacingWidgetBuilder(
+    BuildContext context,
+    List<SpacingType> spacingTypes,
+  ) {
+    final isDefaultSpacing = spacingTypes.contains(SpacingType.defaultSpacing);
+    if (spacingTypes.length == 1 && isDefaultSpacing) {
+      return const SizedBox(height: 2);
+    }
+    return const SizedBox(height: 8);
+  }
 
   @override
   _StreamMessageListViewState createState() => _StreamMessageListViewState();
@@ -612,7 +623,6 @@ class _StreamMessageListViewState extends State<StreamMessageListView> {
                     }
 
                     late final Message message, nextMessage;
-                    Widget? separator;
                     if (widget.reverse) {
                       message = messages[i - 1];
                       nextMessage = messages[i - 2];
@@ -625,15 +635,14 @@ class _StreamMessageListViewState extends State<StreamMessageListView> {
                       nextMessage.createdAt.toLocal(),
                       Units.DAY,
                     )) {
-                      separator = _buildDateDivider(nextMessage);
+                      return _buildDateDivider(nextMessage);
                     }
+
                     final timeDiff =
                         Jiffy(nextMessage.createdAt.toLocal()).diff(
                       message.createdAt.toLocal(),
                       Units.MINUTE,
                     );
-
-                    final spacingRules = <SpacingType>[];
 
                     final isNextUserSame =
                         message.user!.id == nextMessage.user?.id;
@@ -641,64 +650,31 @@ class _StreamMessageListViewState extends State<StreamMessageListView> {
                     final isDeleted = message.isDeleted;
                     final hasTimeDiff = timeDiff >= 1;
 
-                    if (hasTimeDiff) {
-                      spacingRules.add(SpacingType.timeDiff);
+                    final spacingRules = [
+                      if (hasTimeDiff) SpacingType.timeDiff,
+                      if (!isNextUserSame) SpacingType.otherUser,
+                      if (isThread) SpacingType.thread,
+                      if (isDeleted) SpacingType.deleted,
+                    ];
+
+                    if (spacingRules.isEmpty) {
+                      spacingRules.add(SpacingType.defaultSpacing);
                     }
 
-                    if (!isNextUserSame) {
-                      spacingRules.add(SpacingType.otherUser);
-                    }
-
-                    if (isThread) {
-                      spacingRules.add(SpacingType.thread);
-                    }
-
-                    if (isDeleted) {
-                      spacingRules.add(SpacingType.deleted);
-                    }
-
-                    if (spacingRules.isNotEmpty) {
-                      separator ??= widget.spacingWidgetBuilder
-                              ?.call(context, spacingRules) ??
-                          const SizedBox(height: 8);
-                    }
-                    separator ??= widget.spacingWidgetBuilder
-                            ?.call(context, [SpacingType.defaultSpacing]) ??
-                        const SizedBox(height: 2);
+                    final separator = widget.spacingWidgetBuilder.call(
+                      context,
+                      spacingRules,
+                    );
 
                     if (!isThread && unreadCount > 0 && unreadCount == i - 1) {
-                      final unreadMessagesSeparator = widget
-                          .unreadMessagesSeparatorBuilder
-                          ?.call(context, unreadCount);
+                      final unreadMessagesSeparator =
+                          _buildUnreadMessagesSeparator(unreadCount);
 
                       return Column(
                         crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
                           separator,
-                          unreadMessagesSeparator ??
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 8),
-                                child: DecoratedBox(
-                                  decoration: BoxDecoration(
-                                    gradient:
-                                        _streamTheme.colorTheme.bgGradient,
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8),
-                                    child: Text(
-                                      context.translations
-                                          .unreadMessagesSeparatorText(
-                                        unreadCount,
-                                      ),
-                                      textAlign: TextAlign.center,
-                                      style:
-                                          StreamChannelHeaderTheme.of(context)
-                                              .subtitleStyle,
-                                    ),
-                                  ),
-                                ),
-                              ),
+                          unreadMessagesSeparator,
                         ],
                       );
                     }
@@ -814,6 +790,30 @@ class _StreamMessageListViewState extends State<StreamMessageListView> {
     }
 
     return child;
+  }
+
+  Widget _buildUnreadMessagesSeparator(int unreadCount) {
+    final unreadMessagesSeparator =
+        widget.unreadMessagesSeparatorBuilder?.call(context, unreadCount) ??
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: _streamTheme.colorTheme.bgGradient,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Text(
+                    context.translations.unreadMessagesSeparatorText(
+                      unreadCount,
+                    ),
+                    textAlign: TextAlign.center,
+                    style: StreamChannelHeaderTheme.of(context).subtitleStyle,
+                  ),
+                ),
+              ),
+            );
+    return unreadMessagesSeparator;
   }
 
   Widget _buildDateDivider(Message message) {
