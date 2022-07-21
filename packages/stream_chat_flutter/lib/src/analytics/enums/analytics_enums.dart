bafeec722cfe319f6fea169efb0686ddeb6c3daf
Ayush Shekhar <ayush221b@gmail.com>
2022-07-21 11:33:27 +0530

    Resolved the circular export error by upgrading freezed and build runner

--- packages/stream_chat_flutter/lib/src/analytics/enums/analytics_enums.dart
+++ packages/stream_chat_flutter/lib/src/analytics/enums/analytics_enums.dart
@@ -11,5 +11,38 @@ enum StreamMessageInputAttachmentActionType {
   capturePhoto,
 
   /// Use the camera to record a video
-  captureVideo
+  captureVideo,
+}
+
+/// The types of actions that can be taken on a message
+enum StreamMessageActionType {
+  /// directly quote and reply to the message
+  reply,
+
+  /// reply to the message and start a thread
+  threadReply,
+
+  /// flag the message
+  flag,
+
+  /// pin message to the channel
+  pin,
+
+  /// unpin the message from the channel
+  unpin,
+
+  /// delete the message from the channel
+  delete,
+}
+
+/// The actions that can be taken when an attachment is open in full screen view
+enum StreamAttachmentFullScreenActionType {
+  /// Reply to the message containing this attachment
+  reply,
+
+  /// Show the message containing this attachment in the message listview
+  showInChat,
+
+  /// Save this attachment to device
+  save,
 }
--- packages/stream_chat_flutter/lib/src/analytics/models/stream_chat_analytics_event.dart
+++ packages/stream_chat_flutter/lib/src/analytics/models/stream_chat_analytics_event.dart
@@ -76,6 +76,240 @@ class StreamChatAnalyticsEvent with _$StreamChatAnalyticsEvent {
     required StreamMessageInputAttachmentActionType targetActionType,
   }) = StreamMessageInputAttachmentActionClickEventModel;
 
+  /// This event is triggered the back button is pressed on a channel header
+  const factory StreamChatAnalyticsEvent.channelHeaderBackButtonClick({
+    /// id of the channel on which back button was pressed
+    required String channelId,
+
+    /// the number of unread messages visible on the back button when
+    /// it was pressed
+    required int unreadMessageCount,
+  }) = StreamChannelHeaderBackButtonClickEventModel;
+
+  /// This event is triggered the avatar is pressed on a channel header
+  const factory StreamChatAnalyticsEvent.channelHeaderAvatarClick({
+    /// id of the channel
+    required String channelId,
+  }) = StreamChannelHeaderAvatarClickEventModel;
+
+  /// This event is triggered the first time a message list is loaded
+  const factory StreamChatAnalyticsEvent.messageListView({
+    /// id of the channel
+    required String channelId,
+  }) = StreamMessageListViewEventModel;
+
+  /// This event is triggered when the scrollToBottom button is clicked
+  /// in the message listview
+  const factory StreamChatAnalyticsEvent.messageListScrollToBottomClick({
+    /// id of the channel
+    required String channelId,
+  }) = StreamMessageListScrollToBottomClickEventModel;
+
+  /// This event is triggered when a user mention is clicked
+  const factory StreamChatAnalyticsEvent.messageMentionClick({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// id of the mentioned user
+    required String mentionedUserId,
+
+    /// id of the message to which this message is a direct reply
+    String? quotedMessageId,
+
+    /// id of the parent message in this thread
+    String? parentMessageId,
+  }) = StreamMessageMentionClickEventModel;
+
+  /// This event is triggered when a URL in a message is clicked
+  const factory StreamChatAnalyticsEvent.messageUrlClick({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// id of the message to which this message is a direct reply
+    String? quotedMessageId,
+
+    /// id of the parent message in this thread
+    String? parentMessageId,
+  }) = StreamMessageUrlClickEventModel;
+
+  /// This event is triggered when the actions that can be taken on a message
+  /// are accessed
+  const factory StreamChatAnalyticsEvent.messageActionsTrigger({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// id of the message to which this message is a direct reply
+    String? quotedMessageId,
+
+    /// id of the parent message in this thread
+    String? parentMessageId,
+  }) = StreamMessageActionsTriggerEventModel;
+
+  /// This event is triggered when a reaction is selected for a message
+  const factory StreamChatAnalyticsEvent.messageReactionClick({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// id of the message to which this message is a direct reply
+    String? quotedMessageId,
+
+    /// id of the parent message in this thread
+    String? parentMessageId,
+
+    /// the type of reaction that was selected
+    required String reactionType,
+  }) = StreamMessageReactionClickEventModel;
+
+  /// This event is triggered when a specific type of action
+  /// is selected for a message
+  const factory StreamChatAnalyticsEvent.messageActionClick({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// id of the message to which this message is a direct reply
+    String? quotedMessageId,
+
+    /// id of the parent message in this thread
+    String? parentMessageId,
+
+    /// the type of action that was selected
+    required StreamMessageActionType actionType,
+  }) = StreamMessageActionClickEventModel;
+
+  /// This event is triggered when an attachment in a message is clicked
+  const factory StreamChatAnalyticsEvent.messageAttachmentClick({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// id of the message to which this message is a direct reply
+    String? quotedMessageId,
+
+    /// id of the parent message in this thread
+    String? parentMessageId,
+
+    /// the id of the attachment
+    required String attachmentId,
+
+    /// the type of the attachment
+    required String attachmentType,
+  }) = StreamMessageAttachmentClickEventModel;
+
+  /// This event is triggered when an attachment is opened in full screen
+  const factory StreamChatAnalyticsEvent.attachmentFullScreenView({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// the id of the attachment
+    required String attachmentId,
+
+    /// the type of the attachment
+    required String attachmentType,
+  }) = StreamAttachmentFullScreenViewEventModel;
+
+  /// This event is triggered when an attachment is opened in full screen
+  /// and the share button is clicked
+  const factory StreamChatAnalyticsEvent.attachmentFullScreenShareButtonClick({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// the id of the attachment
+    required String attachmentId,
+
+    /// the type of the attachment
+    required String attachmentType,
+  }) = StreamAttachmentFullScreenShareButtonClickEventModel;
+
+  /// This event is triggered when an attachment is opened in full screen
+  /// and the grid button is clicked
+  const factory StreamChatAnalyticsEvent.attachmentFullScreenGridButtonClick({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// the id of the attachment
+    required String attachmentId,
+
+    /// the type of the attachment
+    required String attachmentType,
+  }) = StreamAttachmentFullScreenGridButtonClickEventModel;
+
+  /// This event is triggered when an attachment is opened in full screen
+  /// and the close button is clicked
+  const factory StreamChatAnalyticsEvent.attachmentFullScreenCloseButtonClick({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// the id of the attachment
+    required String attachmentId,
+
+    /// the type of the attachment
+    required String attachmentType,
+  }) = StreamAttachmentFullScreenCloseButtonClickEventModel;
+
+  /// This event is triggered when an attachment is opened in full screen
+  /// and the actions menu is clicked
+  const factory StreamChatAnalyticsEvent.attachmentFullScreenActionsMenuClick({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// the id of the attachment
+    required String attachmentId,
+
+    /// the type of the attachment
+    required String attachmentType,
+  }) = StreamAttachmentFullScreenActionsMenuClickEventModel;
+
+  /// This event is triggered when an attachment is opened in full screen
+  /// and an action menu item is clicked
+  const factory StreamChatAnalyticsEvent.attachmentFullScreenActionItemClick({
+    /// id of the channel
+    required String channelId,
+
+    /// id of the message
+    required String messageId,
+
+    /// the id of the attachment
+    required String attachmentId,
+
+    /// the type of the attachment
+    required String attachmentType,
+
+    /// the type of action that was selected
+    required StreamAttachmentFullScreenActionType actionType,
+  }) = StreamAttachmentFullScreenActionItemClickEventModel;
+
   /// Create an instance of [StreamChatAnalyticsEvent] from a map
   factory StreamChatAnalyticsEvent.fromJson(Map<String, dynamic> json) =>
       _$StreamChatAnalyticsEventFromJson(json);
