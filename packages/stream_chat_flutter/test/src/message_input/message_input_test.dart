f7bb9028611d5fa24f5f4ba378bb7cebdb9c2418
Leandro Borges Ferreira <lehen01@gmail.com>
2023-03-23 15:49:59 +0100

    Adding mocked test

--- packages/stream_chat_flutter/test/src/message_input/message_input_test.dart
+++ packages/stream_chat_flutter/test/src/message_input/message_input_test.dart
@@ -285,6 +285,96 @@ void main() {
     },
   );
 
+  // testWidgets(
+  //   'Test that start the audio input is correctly appears',
+  //   (WidgetTester tester) async {
+  //
+  //     final client = MockClient();
+  //     final clientState = MockClientState();
+  //     final channel = MockChannel();
+  //     final channelState = MockChannelState();
+  //     final lastMessageAt = DateTime.parse('2020-06-22 12:00:00');
+  //     final mockRecordController = MockStreamRecordController();
+  //
+  //     when(() => client.state).thenReturn(clientState);
+  //     when(() => clientState.currentUser).thenReturn(OwnUser(id: 'user-id'));
+  //     when(() => channel.lastMessageAt).thenReturn(lastMessageAt);
+  //     when(() => channel.state).thenReturn(channelState);
+  //     when(() => channel.client).thenReturn(client);
+  //     when(() => channel.isMuted).thenReturn(false);
+  //     when(() => channel.isMutedStream).thenAnswer((i) => Stream.value(false));
+  //     when(() => channel.extraDataStream).thenAnswer(
+  //       (i) => Stream.value({
+  //         'name': 'test',
+  //       }),
+  //     );
+  //     when(() => channel.extraData).thenReturn({
+  //       'name': 'test',
+  //     });
+  //     when(() => channelState.membersStream).thenAnswer(
+  //       (i) => Stream.value([
+  //         Member(
+  //           userId: 'user-id',
+  //           user: User(id: 'user-id'),
+  //         )
+  //       ]),
+  //     );
+  //     when(() => channelState.members).thenReturn([
+  //       Member(
+  //         userId: 'user-id',
+  //         user: User(id: 'user-id'),
+  //       ),
+  //     ]);
+  //     when(() => channelState.messages).thenReturn([
+  //       Message(
+  //         text: 'hello',
+  //         user: User(id: 'other-user'),
+  //       )
+  //     ]);
+  //     when(() => channelState.messagesStream).thenAnswer(
+  //       (i) => Stream.value([
+  //         Message(
+  //           text: 'hello',
+  //           user: User(id: 'other-user'),
+  //         )
+  //       ]),
+  //     );
+  //
+  //     final recordStateController = BehaviorSubject<RecordState>();
+  //     when(() => mockRecordController.recordState)
+  //         .thenAnswer((_) => recordStateController.stream);
+  //
+  //     await tester.pumpWidget(MaterialApp(
+  //       home: StreamChat(
+  //         client: client,
+  //         child: StreamChannel(
+  //           channel: channel,
+  //           child: Scaffold(
+  //             body: StreamMessageInput(
+  //               enableAudioRecord: false,
+  //               // recordController: mockRecordController,
+  //             ),
+  //           ),
+  //         ),
+  //       ),
+  //     ));
+  //
+  //     await tester.runAsync(() async {
+  //       recordStateController.add(RecordState.record);
+  //
+  //       await Future<void>.delayed(const Duration(milliseconds: 100));
+  //
+  //       // expect(find.byKey(const Key('startRecord')), findsNothing);
+  //       // expect(
+  //       //   find.byWidgetPredicate((widget) => widget is RecordField),
+  //       //   findsOneWidget,
+  //       // );
+  //       recordStateController.close();
+  //     }
+  //     );
+  //   },
+  // );
+
   testWidgets(
     'checks message input slow mode',
     (WidgetTester tester) async {
