79b260d3c56b46635a65a086290cef6791273f7a
Sahil Kumar <xdsahil@gmail.com>
2023-04-27 14:36:21 +0530

    test: add more tests
    Signed-off-by: xsahil03x <xdsahil@gmail.com>

--- packages/stream_chat_flutter/test/src/utils/extension_test.dart
+++ packages/stream_chat_flutter/test/src/utils/extension_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
@@ -89,7 +88,62 @@ void main() {
       expect('☺️'.isOnlyEmoji, true);
     });
 
-    test('korean symbols', () {
+    test('Korean vowels', () {
+      expect('ㅏ'.isOnlyEmoji, false);
+      expect('ㅑ'.isOnlyEmoji, false);
+      expect('ㅓ'.isOnlyEmoji, false);
+      expect('ㅕ'.isOnlyEmoji, false);
+      expect('ㅗ'.isOnlyEmoji, false);
+      expect('ㅛ'.isOnlyEmoji, false);
+      expect('ㅜ'.isOnlyEmoji, false);
+      expect('ㅠ'.isOnlyEmoji, false);
+      expect('ㅡ'.isOnlyEmoji, false);
+      expect('ㅣ'.isOnlyEmoji, false);
+    });
+
+    test('Korean consonants', () {
+      expect('ㄱ'.isOnlyEmoji, false);
+      expect('ㄴ'.isOnlyEmoji, false);
+      expect('ㄷ'.isOnlyEmoji, false);
+      expect('ㄹ'.isOnlyEmoji, false);
+      expect('ㅁ'.isOnlyEmoji, false);
+      expect('ㅂ'.isOnlyEmoji, false);
+      expect('ㅅ'.isOnlyEmoji, false);
+      expect('ㅇ'.isOnlyEmoji, false);
+      expect('ㅈ'.isOnlyEmoji, false);
+      expect('ㅊ'.isOnlyEmoji, false);
+      expect('ㅋ'.isOnlyEmoji, false);
+      expect('ㅌ'.isOnlyEmoji, false);
+      expect('ㅍ'.isOnlyEmoji, false);
+      expect('ㅎ'.isOnlyEmoji, false);
+    });
+
+    test('Korean syllables', () {
+      expect('가'.isOnlyEmoji, false);
+      expect('나'.isOnlyEmoji, false);
+      expect('다'.isOnlyEmoji, false);
+      expect('라'.isOnlyEmoji, false);
+      expect('마'.isOnlyEmoji, false);
+      expect('바'.isOnlyEmoji, false);
+      expect('사'.isOnlyEmoji, false);
+      expect('아'.isOnlyEmoji, false);
+      expect('자'.isOnlyEmoji, false);
+      expect('차'.isOnlyEmoji, false);
+      expect('카'.isOnlyEmoji, false);
+      expect('타'.isOnlyEmoji, false);
+      expect('파'.isOnlyEmoji, false);
+      expect('하'.isOnlyEmoji, false);
+    });
+
+    // https://github.com/GetStream/stream-chat-flutter/issues/1502
+    test('Issue:#1502', () {
+      expect('ㄴ'.isOnlyEmoji, false);
+      expect('ㄴㅇ'.isOnlyEmoji, false);
+      expect('ㅇㅋ'.isOnlyEmoji, false);
+    });
+
+    // https://github.com/GetStream/stream-chat-flutter/issues/1505
+    test('Issue:#1505', () {
       expect('ㅎㅎㅎ'.isOnlyEmoji, false);
       expect('ㅎㅎㅎㅎ'.isOnlyEmoji, false);
     });
