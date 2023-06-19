7adc99fecb982cd8ba1fa37ab97db76cdd966f08
Sahil Kumar <xdsahil@gmail.com>
2023-06-19 15:53:34 +0530

    feat(ui): add support for `StreamMessageInput.contentInsertionConfiguration`.
    Signed-off-by: xsahil03x <xdsahil@gmail.com>
    (cherry picked from commit bdc85b48730475ff3f9f5b5c36405ea373705812)

--- packages/stream_chat_flutter/lib/src/message_input/stream_message_input.dart
+++ packages/stream_chat_flutter/lib/src/message_input/stream_message_input.dart
@@ -147,6 +147,7 @@ class StreamMessageInput extends StatefulWidget {
         _defaultClearQuotedMessageKeyPredicate,
     this.ogPreviewFilter = _defaultOgPreviewFilter,
     this.hintGetter = _defaultHintGetter,
+    this.contentInsertionConfiguration,
   });
 
   /// The predicate used to send a message on desktop/web
@@ -339,6 +340,9 @@ class StreamMessageInput extends StatefulWidget {
 
   /// Returns the hint text for the message input.
   final HintGetter hintGetter;
+
+  /// {@macro flutter.widgets.editableText.contentInsertionConfiguration}
+  final ContentInsertionConfiguration? contentInsertionConfiguration;
 
   static String? _defaultHintGetter(
     BuildContext context,
@@ -905,6 +909,8 @@ class StreamMessageInputState extends State<StreamMessageInput>
                         decoration: _getInputDecoration(context),
                         textCapitalization: widget.textCapitalization,
                         autocorrect: widget.autoCorrect,
+                        contentInsertionConfiguration:
+                            widget.contentInsertionConfiguration,
                       ),
                     ),
                   ),
