015628bd7bad70ed6687a4b42f1cb4f2d23634bb
Sahil Kumar <xdsahil@gmail.com>
2023-06-22 14:12:29 +0530

    chore(localization): bump stream_chat_flutter
    Signed-off-by: xsahil03x <xdsahil@gmail.com>
    (cherry picked from commit 677b71e728b095e6f2088aa1a694846c26bcdb7e)

--- packages/stream_chat_localizations/lib/src/stream_chat_localizations_hi.dart
+++ packages/stream_chat_localizations/lib/src/stream_chat_localizations_hi.dart
@@ -243,13 +243,15 @@ class StreamChatLocalizationsHi extends GlobalStreamChatLocalizations {
     } else if (date == yesterday) {
       return 'कल';
     } else {
-      return '${Jiffy(date).MMMd} को';
+      return '${Jiffy.parseFromDateTime(date).MMMd} को';
     }
   }
 
   @override
-  String sentAtText({required DateTime date, required DateTime time}) =>
-      '${_getDay(date)} ${Jiffy(time.toLocal()).format('HH:mm')} बजे भेजा गया';
+  String sentAtText({required DateTime date, required DateTime time}) {
+    final atTime = Jiffy.parseFromDateTime(time.toLocal());
+    return '${_getDay(date)} ${atTime.jm} बजे भेजा गया';
+  }
 
   @override
   String get todayLabel => 'आज';
