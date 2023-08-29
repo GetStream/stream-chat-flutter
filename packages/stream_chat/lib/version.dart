a2a3e51c1a2fe84c5772d98d3bc2ca4e4759737e
Sahil Kumar <xdsahil@gmail.com>
2023-08-30 00:51:12 +0530

    test

--- packages/stream_chat/lib/version.dart
+++ packages/stream_chat/lib/version.dart
@@ -1,6 +1,6 @@
 import 'package:stream_chat/src/client/client.dart';
 
-/// Current package version
+/// The Current package version
 /// Used in [StreamChatClient] to build the `x-stream-client` header
 // ignore: constant_identifier_names
 const PACKAGE_VERSION = '6.8.0';
