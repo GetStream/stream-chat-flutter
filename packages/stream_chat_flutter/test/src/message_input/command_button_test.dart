d2265690a95e3b94b13771c94bc518d90963d9c7
Efthymis Sarmpanis <e.sarbanis@gmail.com>
2024-02-19 13:49:07 +0200

    test(UI): add more coverage in CommandButton (#1850)

--- packages/stream_chat_flutter/test/src/message_input/command_button_test.dart
+++ packages/stream_chat_flutter/test/src/message_input/command_button_test.dart
@@ -27,6 +27,49 @@ void main() {
     expect(count, 1);
   });
 
+  testWidgets('CommandButton should accept icon', (tester) async {
+    const icon = Icon(Icons.add);
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: CommandButton(
+              icon: icon,
+              onPressed: () {},
+            ),
+          ),
+        ),
+      ),
+    );
+
+    final iconFound = find.byWidget(icon);
+    expect(iconFound, findsOneWidget);
+  });
+
+  testWidgets('CommandButton should not accept both color and icon',
+      (tester) async {
+    expect(
+      () => MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: CommandButton(
+              color: Colors.red,
+              icon: const Icon(Icons.add),
+              onPressed: () {},
+            ),
+          ),
+        ),
+      ),
+      throwsA(
+        isA<AssertionError>().having(
+          (e) => e.message,
+          'message',
+          'Either icon or color should be provided',
+        ),
+      ),
+    );
+  });
+
   testGoldens('golden test for CommandButton', (tester) async {
     await tester.pumpWidget(
       MaterialApp(
