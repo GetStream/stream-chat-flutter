1e1d5f0b0d213960571a49e3d405313db1b6a028
Maurizio Pinotti <maurizio.pinotti@cnhind.com>
2024-02-21 13:30:23 +0100

    fix(core): FormatException: unmatched coverage:ignore-end found

--- packages/stream_chat/lib/src/client/client.dart
+++ packages/stream_chat/lib/src/client/client.dart
@@ -1700,8 +1700,6 @@ class ClientState {
   /// The current user as a stream
   Stream<OwnUser?> get currentUserStream => _currentUserController.stream;
 
-  // coverage:ignore-end
-
   /// The current user
   Map<String, User> get users => _usersController.value;
 
