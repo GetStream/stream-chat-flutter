a8803eb8072815c96c7bb13d2bfa8c08d990979b
Alex Tarana <alex.tarana@sentric.it>
2024-06-26 00:49:19 +0200

    switch to media_kit

--- packages/stream_chat_flutter/lib/src/video/vlc/vlc_manager_desktop.dart
+++ packages/stream_chat_flutter/lib/src/video/vlc/vlc_manager_desktop.dart
@@ -1,11 +1,11 @@
-import 'package:dart_vlc/dart_vlc.dart';
+import 'package:media_kit/media_kit.dart';
 import 'package:stream_chat_flutter/src/video/vlc/vlc_manager.dart';
 
 /// The desktop implementation of [VlcManager]. It simply initializes VLC.
 class VlcManagerDesktop extends VlcManager {
   @override
   void initialize() {
-    DartVLC.initialize();
+    MediaKit.ensureInitialized();
   }
 }
 
