20a538da8c8ae27e8796e689ec0fc480aad29c49
Deven Joshi <deven9852@gmail.com>
2024-08-14 14:35:27 +0200

    v8 doc structure creation

--- docusaurus/flutter-docusaurus-dontent-docs.plugin.js
+++ docusaurus/flutter-docusaurus-dontent-docs.plugin.js
@@ -6,20 +6,8 @@ module.exports = {
                 lastVersion: 'current',
                 versions: {
                     current: {
-                        label: 'v7'
+                        label: 'v8'
                     },
-                    '3.x.x': {
-                        label: 'v3',
-                        path: 'v3'
-                    },
-                    '4.x.x': {
-                        label: 'v4',
-                        path: 'v4'
-                    },
-                    '5.x.x': {
-                        label: 'v5',
-                        path: 'v5'
-                    }
                 }
             }
         ]
