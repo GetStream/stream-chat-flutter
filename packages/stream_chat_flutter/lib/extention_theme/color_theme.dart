2e195484b967dca8d83dccb85ad752620f6b668f
nss2000 <nagender@unikon.ai>
2024-10-05 18:36:53 +0530

    UA-618 theme changes

---
+++ packages/stream_chat_flutter/lib/extention_theme/color_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UnikonColorTheme {
+  const UnikonColorTheme._();
+  static const Color messageSentIndicatorColor =
+      Color.fromRGBO(255, 255, 255, 1);
+
+  static const Color replyQuotedMessageBGColor = Color.fromRGBO(49, 49, 49, 1);
+
+  static const Color replyQuotedMessageBGColor2 = Color.fromRGBO(47, 72, 69, 1);
+
+  static const Color darkGreyColor = Color.fromRGBO(34, 34, 34, 1);
+
+  static const Color primaryColor = Color.fromRGBO(20, 143, 128, 1);
+
+  static const Color whiteHintTextColor = Color.fromRGBO(204, 204, 204, 0.8);
+
+  static const Color greyColor = Color.fromRGBO(204, 204, 204, 1.0);
+}
