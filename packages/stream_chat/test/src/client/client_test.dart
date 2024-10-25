8ad085d46f10cafc8eb76696c9241477fe7551cc
Sahil Kumar <sahil@getstream.io>
2024-10-25 15:49:37 +0200

    chore: fix lints

--- packages/stream_chat/test/src/client/client_test.dart
+++ packages/stream_chat/test/src/client/client_test.dart
@@ -1,8 +1,6 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:stream_chat/src/core/http/token.dart';
 import 'package:stream_chat/src/core/models/banned_user.dart';
-import 'package:stream_chat/src/core/models/poll.dart';
-import 'package:stream_chat/src/core/models/poll_option.dart';
 import 'package:stream_chat/src/core/models/user_block.dart';
 import 'package:stream_chat/stream_chat.dart';
 import 'package:test/test.dart';
