921086d87e545f8f617b74ba7ccff70626fa3b15
Sahil Kumar <sahil@getstream.io>
2024-10-25 15:42:22 +0200

    test: fix test

--- packages/stream_chat/test/src/core/models/event_test.dart
+++ packages/stream_chat/test/src/core/models/event_test.dart
@@ -40,6 +40,8 @@ void main() {
           'user': {'id': 'id'},
           'reaction': null,
           'message': null,
+          'poll': null,
+          'poll_vote': null,
           'channel': null,
           'total_unread_count': 1,
           'unread_channels': 1,
