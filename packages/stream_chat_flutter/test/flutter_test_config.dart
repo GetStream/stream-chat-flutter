d1ecb042226604414f37485435d82fecf97afda7
Sahil Kumar <sahil@getstream.io>
2024-12-23 20:49:44 +0100

    test again

--- packages/stream_chat_flutter/test/flutter_test_config.dart
+++ packages/stream_chat_flutter/test/flutter_test_config.dart
@@ -4,16 +4,12 @@ import 'dart:io';
 import 'package:alchemist/alchemist.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
-  final isRunningInCi = const bool.fromEnvironment('CI') ||
-      Platform.environment.containsKey('CI') ||
+  final isRunningInCi = Platform.environment.containsKey('CI') ||
       Platform.environment.containsKey('GITHUB_ACTIONS');
-
-  print('Running in CI: $isRunningInCi');
 
   return AlchemistConfig.runWithConfig(
     config: AlchemistConfig(
       platformGoldensConfig: PlatformGoldensConfig(
-        // ignore: avoid_redundant_argument_values
         enabled: !isRunningInCi,
       ),
     ),
