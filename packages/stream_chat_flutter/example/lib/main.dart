2ecf41b15b66c373e4189aa7d8b9022202a848b8
Sahil Kumar <sahil@getstream.io>
2024-12-24 14:06:15 +0100

    chore: minor changes

--- packages/stream_chat_flutter/example/lib/main.dart
+++ packages/stream_chat_flutter/example/lib/main.dart
@@ -252,93 +252,83 @@ class _ChannelPageState extends State<ChannelPage> {
             body: Column(
               children: <Widget>[
                 Expanded(
-                  child: StreamMessageListViewTheme(
-                    data: StreamMessageListViewThemeData(
-                      backgroundColor: Colors.transparent,
-                    ),
-                    child: StreamMessageListView(
-                      threadBuilder: (context, parent) {
-                        return ThreadPage(
-                          parent: parent!,
-                        );
-                      },
-                      messageBuilder: (
-                        context,
-                        messageDetails,
-                        messages,
-                        defaultWidget,
-                      ) {
-                        // The threshold after which the message is considered
-                        // swiped.
-                        const threshold = 0.2;
+                  child: StreamMessageListView(
+                    threadBuilder: (_, parent) => ThreadPage(parent: parent!),
+                    messageBuilder: (
+                      context,
+                      messageDetails,
+                      messages,
+                      defaultWidget,
+                    ) {
+                      // The threshold after which the message is considered
+                      // swiped.
+                      const threshold = 0.2;
 
-                        final isMyMessage = messageDetails.isMyMessage;
+                      final isMyMessage = messageDetails.isMyMessage;
 
-                        // The direction in which the message can be swiped.
-                        final swipeDirection = isMyMessage
-                            ? SwipeDirection.endToStart //
-                            : SwipeDirection.startToEnd;
+                      // The direction in which the message can be swiped.
+                      final swipeDirection = isMyMessage
+                          ? SwipeDirection.endToStart //
+                          : SwipeDirection.startToEnd;
 
-                        return Swipeable(
-                          key: ValueKey(messageDetails.message.id),
-                          direction: swipeDirection,
-                          swipeThreshold: threshold,
-                          onSwiped: (details) => reply(messageDetails.message),
-                          backgroundBuilder: (context, details) {
-                            // The alignment of the swipe action.
-                            final alignment = isMyMessage
-                                ? Alignment.centerRight //
-                                : Alignment.centerLeft;
+                      return Swipeable(
+                        key: ValueKey(messageDetails.message.id),
+                        direction: swipeDirection,
+                        swipeThreshold: threshold,
+                        onSwiped: (details) => reply(messageDetails.message),
+                        backgroundBuilder: (context, details) {
+                          // The alignment of the swipe action.
+                          final alignment = isMyMessage
+                              ? Alignment.centerRight //
+                              : Alignment.centerLeft;
 
-                            // The progress of the swipe action.
-                            final progress =
-                                math.min(details.progress, threshold) /
-                                    threshold;
+                          // The progress of the swipe action.
+                          final progress =
+                              math.min(details.progress, threshold) / threshold;
 
-                            // The offset for the reply icon.
-                            var offset = Offset.lerp(
-                              const Offset(-24, 0),
-                              const Offset(12, 0),
-                              progress,
-                            )!;
+                          // The offset for the reply icon.
+                          var offset = Offset.lerp(
+                            const Offset(-24, 0),
+                            const Offset(12, 0),
+                            progress,
+                          )!;
 
-                            // If the message is mine, we need to flip the offset.
-                            if (isMyMessage) {
-                              offset = Offset(-offset.dx, -offset.dy);
-                            }
+                          // If the message is mine, we need to flip the offset.
+                          if (isMyMessage) {
+                            offset = Offset(-offset.dx, -offset.dy);
+                          }
 
-                            final _streamTheme = StreamChatTheme.of(context);
+                          final _streamTheme = StreamChatTheme.of(context);
 
-                            return Align(
-                              alignment: alignment,
-                              child: Transform.translate(
-                                offset: offset,
-                                child: Opacity(
-                                  opacity: progress,
-                                  child: SizedBox.square(
-                                    dimension: 30,
-                                    child: CustomPaint(
-                                      painter: AnimatedCircleBorderPainter(
-                                        progress: progress,
-                                        color: _streamTheme.colorTheme.borders,
-                                      ),
-                                      child: Center(
-                                        child: StreamSvgIcon.reply(
-                                          size: lerpDouble(0, 18, progress),
-                                          color: _streamTheme
-                                              .colorTheme.accentPrimary,
-                                        ),
+                          return Align(
+                            alignment: alignment,
+                            child: Transform.translate(
+                              offset: offset,
+                              child: Opacity(
+                                opacity: progress,
+                                child: SizedBox.square(
+                                  dimension: 30,
+                                  child: CustomPaint(
+                                    painter: AnimatedCircleBorderPainter(
+                                      progress: progress,
+                                      color: _streamTheme.colorTheme.borders,
+                                    ),
+                                    child: Center(
+                                      child: StreamSvgIcon.reply(
+                                        size: lerpDouble(0, 18, progress),
+                                        color: _streamTheme
+                                            .colorTheme.accentPrimary,
                                       ),
                                     ),
                                   ),
                                 ),
                               ),
-                            );
-                          },
-                          child: defaultWidget.copyWith(onReplyTap: reply),
-                        );
-                      },
-                    ),
+                            ),
+                          );
+                        },
+                        child: defaultWidget.copyWith(onReplyTap: reply),
+                      );
+                    },
                   ),
                 ),
                 StreamMessageInput(
