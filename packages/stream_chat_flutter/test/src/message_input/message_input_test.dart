c938294acd85a1fca03a81a2b75aea4e8b0c0ffd
Sahil Kumar <sahil@getstream.io>
2025-03-29 02:13:28 +0100

    test: add sendPredicate test

--- packages/stream_chat_flutter/test/src/message_input/message_input_test.dart
+++ packages/stream_chat_flutter/test/src/message_input/message_input_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
@@ -154,4 +155,127 @@ void main() {
       expect(find.text('Slow mode ON'), findsOneWidget);
     },
   );
+
+  group('MessageInput keyboard interactions', () {
+    final client = MockClient();
+    final clientState = MockClientState();
+    final channel = MockChannel();
+    final channelState = MockChannelState();
+
+    setUp(() {
+      registerFallbackValue(Message());
+
+      when(() => client.state).thenReturn(clientState);
+      when(() => clientState.currentUser).thenReturn(OwnUser(id: 'user-id'));
+      when(() => channel.state).thenReturn(channelState);
+      when(() => channel.client).thenReturn(client);
+      when(channel.getRemainingCooldown).thenReturn(0);
+      when(() => channelState.isUpToDate).thenReturn(true);
+    });
+
+    testWidgets(
+      'should send message when Enter key is pressed on desktop',
+      (tester) async {
+        when(() => channel.sendMessage(any())).thenAnswer(
+          (i) async => SendMessageResponse()
+            ..message = Message(
+              text: 'Hello world',
+            ),
+        );
+
+        await tester.pumpWidget(
+          MaterialApp(
+            home: StreamChat(
+              client: client,
+              child: StreamChannel(
+                channel: channel,
+                child: const Scaffold(
+                  bottomNavigationBar: StreamMessageInput(),
+                ),
+              ),
+            ),
+          ),
+        );
+
+        await tester.pumpAndSettle();
+
+        // Add some text to the input field
+        final textField = find.byType(StreamMessageInput);
+        await tester.enterText(textField, 'Hello world');
+        await tester.pump();
+
+        // Simulate pressing Enter key
+        await simulateKeyDownEvent(LogicalKeyboardKey.enter, tester: tester);
+
+        await tester.pumpAndSettle();
+
+        // Verify that the message was sent
+        verify(() => channel.sendMessage(any())).called(1);
+      },
+    );
+
+    testWidgets(
+      'should not send message when Shift+Enter key is pressed on desktop',
+      (tester) async {
+        when(() => channel.sendMessage(any())).thenAnswer(
+          (_) async => SendMessageResponse()
+            ..message = Message(
+              text: 'Hello world',
+            ),
+        );
+
+        await tester.pumpWidget(
+          MaterialApp(
+            home: StreamChat(
+              client: client,
+              child: StreamChannel(
+                channel: channel,
+                child: const Scaffold(
+                  bottomNavigationBar: StreamMessageInput(),
+                ),
+              ),
+            ),
+          ),
+        );
+
+        await tester.pumpAndSettle();
+
+        // Add some text to the input field
+        final textField = find.byType(StreamMessageInput);
+        await tester.enterText(textField, 'Hello world');
+        await tester.pump();
+
+        // Simulate pressing Shift+Enter key
+        await simulateKeyDownEvent(
+          LogicalKeyboardKey.enter,
+          tester: tester,
+          isShiftPressed: true,
+        );
+
+        await tester.pumpAndSettle();
+
+        // Verify that the message was not sent.
+        verifyNever(() => channel.sendMessage(any()));
+      },
+    );
+  });
+}
+
+// Helper function to simulate key press events
+Future<void> simulateKeyDownEvent(
+  LogicalKeyboardKey key, {
+  required WidgetTester tester,
+  bool isShiftPressed = false,
+}) async {
+  if (isShiftPressed) {
+    await tester.sendKeyDownEvent(LogicalKeyboardKey.shift);
+  }
+
+  await tester.sendKeyDownEvent(key);
+  await tester.pumpAndSettle();
+  await tester.sendKeyUpEvent(key);
+
+  if (isShiftPressed) {
+    await tester.sendKeyUpEvent(LogicalKeyboardKey.shift);
+  }
 }
