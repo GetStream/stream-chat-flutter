404ea7881cc4501ed6a2ef35d75c9d9c2818ed07
Sahil Kumar <sahil@getstream.io>
2025-07-22 18:06:37 +0200

    chore: update doc

--- packages/stream_chat_persistence/lib/src/dao/draft_message_dao.dart
+++ packages/stream_chat_persistence/lib/src/dao/draft_message_dao.dart
@@ -103,8 +103,8 @@ class DraftMessageDao extends DatabaseAccessor<DriftChatDatabase>
     return query.go();
   }
 
-  /// Deletes all the poll votes whose [DraftMessages.channelCid] is
-  /// present in [cids]
+  /// Deletes all the draft messages by matching [DraftMessages.channelCid]
+  /// with the given list of [cids].
   Future<void> deleteDraftMessagesByCids(List<String> cids) =>
       (delete(draftMessages)..where((tbl) => tbl.channelCid.isIn(cids))).go();
 }
