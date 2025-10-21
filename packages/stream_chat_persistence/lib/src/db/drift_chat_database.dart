a73d08bbb21d87dc3b3520613c211c1a7b442e7d
Sahil Kumar <sahil@getstream.io>
2025-10-21 15:03:17 +0200

    chore(persistence): bump schema version
    Increments the database schema version from 26 to 27.

--- packages/stream_chat_persistence/lib/src/db/drift_chat_database.dart
+++ packages/stream_chat_persistence/lib/src/db/drift_chat_database.dart
@@ -57,7 +57,7 @@ class DriftChatDatabase extends _$DriftChatDatabase {
 
   // you should bump this number whenever you change or add a table definition.
   @override
-  int get schemaVersion => 1000 + 26;
+  int get schemaVersion => 1000 + 27;
 
   @override
   MigrationStrategy get migration => MigrationStrategy(
