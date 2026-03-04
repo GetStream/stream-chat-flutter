4f7a94d0c6922bb5fc65bdff00c3e6601f83f0f8
Rene Floor <rene.floor@getstream.io>
2026-03-04 15:29:44 +0100

    remove unused import

--- packages/stream_chat_flutter/lib/src/components/message_composer/message_composer_extensions.dart
+++ packages/stream_chat_flutter/lib/src/components/message_composer/message_composer_extensions.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
-import 'package:stream_core_flutter/stream_core_flutter.dart';
 
 /// Helper extensions for internal use only.
 extension MessageComposerBuilderExtensions on BuildContext {
