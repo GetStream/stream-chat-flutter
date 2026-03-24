d4f5e0ba6633622ac78a71f37f7f7bc8eaad19de
Rene Floor <rene.floor@getstream.io>
2026-03-24 14:56:15 +0100

    cleanup

--- packages/stream_chat_flutter/lib/src/message_input/stream_message_composer_attachment.dart
+++ packages/stream_chat_flutter/lib/src/message_input/stream_message_composer_attachment.dart
@@ -17,7 +17,8 @@ class StreamMessageComposerAttachment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return context.chatComponentBuilder<StreamMessageComposerAttachmentProps>()?.call(context, props) ??
+        DefaultMessageComposerAttachment(props: props);
   }
 }
 
