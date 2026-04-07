c43b471093b5d6f6546f9f2b32d08d995424dbdc
Rene Floor <rene.floor@getstream.io>
2026-04-07 16:05:35 +0200

    update voice recording screenshots

--- packages/docs_screenshots/test/voice_recording/voice_recording_test.dart
+++ packages/docs_screenshots/test/voice_recording/voice_recording_test.dart
@@ -4,34 +4,24 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:record/record.dart';
 import 'package:stream_chat_flutter/src/audio/audio_playlist_state.dart';
+import 'package:stream_chat_flutter/src/components/message_composer/message_composer_recording_locked.dart';
+import 'package:stream_chat_flutter/src/components/message_composer/message_composer_recording_ongoing.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
 import '../src/fakes.dart';
 import '../src/golden_theme.dart';
 import '../src/mocks.dart';
 
-Widget _buildRecorderButtonScaffold({
-  required MockClient client,
-  required MockChannel channel,
-  required AudioRecorderState recordState,
-}) {
-  return MaterialApp(
-    theme: docsScreenshotsTheme(),
-    debugShowCheckedModeBanner: false,
-    home: StreamChat(
-      client: client,
-      streamChatThemeData: docsStreamChatThemeData(),
-      connectivityStream: Stream.value([ConnectivityResult.mobile]),
-      child: StreamChannel(
-        showLoading: false,
-        channel: channel,
-        child: Scaffold(
-          body: Center(
-            child: StreamAudioRecorderButton(recordState: recordState),
-          ),
-        ),
-      ),
-    ),
+class _MockAudioRecorder extends Mock implements AudioRecorder {}
+
+StreamAudioRecorderController _makeRecorderController(AudioRecorderState initialState) {
+  final mockRecorder = _MockAudioRecorder();
+  when(() => mockRecorder.onAmplitudeChanged(any())).thenAnswer((_) => const Stream.empty());
+  when(() => mockRecorder.dispose()).thenAnswer((_) async {});
+  return StreamAudioRecorderController.raw(
+    config: const RecordConfig(numChannels: 1),
+    recorder: mockRecorder,
+    initialState: initialState,
   );
 }
 
@@ -62,6 +52,27 @@ Widget _buildVoiceRecordingMessageInputScaffold({
   );
 }
 
+Widget _buildVoiceRecorderScaffold({
+  required MockClient client,
+  required Widget child,
+}) {
+  return MaterialApp(
+    theme: docsScreenshotsTheme(),
+    debugShowCheckedModeBanner: false,
+    home: StreamChat(
+      client: client,
+      streamChatThemeData: docsStreamChatThemeData(),
+      connectivityStream: Stream.value([ConnectivityResult.mobile]),
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Center(child: child),
+        ),
+      ),
+    ),
+  );
+}
+
 void _setupChannel(MockClient client, MockClientState clientState, MockChannel channel, MockChannelState channelState) {
   setupMockChannel(
     client: client,
@@ -73,6 +84,10 @@ void _setupChannel(MockClient client, MockClientState clientState, MockChannel c
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() {
+    registerFallbackValue(Duration.zero);
+  });
 
   final originalRecordPlatform = RecordPlatform.instance;
   setUp(() => RecordPlatform.instance = FakeRecordPlatform());
@@ -92,25 +107,6 @@ void main() {
       return _buildVoiceRecordingMessageInputScaffold(
         client: client,
         channel: channel,
-      );
-    },
-  );
-
-  goldenTest(
-    'voice recording idle with tooltip',
-    fileName: 'voice_recording_idle_tooltip',
-    constraints: const BoxConstraints.tightFor(width: 375, height: 80),
-    builder: () {
-      final client = MockClient();
-      final clientState = MockClientState();
-      final channel = MockChannel();
-      final channelState = MockChannelState();
-      _setupChannel(client, clientState, channel, channelState);
-
-      return _buildRecorderButtonScaffold(
-        client: client,
-        channel: channel,
-        recordState: const RecordStateIdle(message: 'Hold to record'),
       );
     },
   );
@@ -136,20 +132,19 @@ void main() {
   goldenTest(
     'voice recording hold recording state',
     fileName: 'voice_recording_hold_recording',
-    constraints: const BoxConstraints.tightFor(width: 375, height: 80),
+    constraints: const BoxConstraints.tightFor(width: 375, height: 56),
     builder: () {
       final client = MockClient();
-      final clientState = MockClientState();
-      final channel = MockChannel();
-      final channelState = MockChannelState();
-      _setupChannel(client, clientState, channel, channelState);
 
-      return _buildRecorderButtonScaffold(
+      final holdState = RecordStateRecordingHold(
+        duration: const Duration(seconds: 5),
+        waveform: List.generate(20, (i) => (i % 5) / 5.0),
+      );
+
+      return _buildVoiceRecorderScaffold(
         client: client,
-        channel: channel,
-        recordState: RecordStateRecordingHold(
-          duration: const Duration(seconds: 5),
-          waveform: List.generate(20, (i) => (i % 5) / 5.0),
+        child: StreamMessageComposerRecordingOngoing(
+          audioRecorderController: _makeRecorderController(holdState),
         ),
       );
     },
@@ -158,20 +153,23 @@ void main() {
   goldenTest(
     'voice recording locked recording state',
     fileName: 'voice_recording_locked_recording',
-    constraints: const BoxConstraints.tightFor(width: 375, height: 80),
+    constraints: const BoxConstraints.tightFor(width: 375, height: 120),
     builder: () {
       final client = MockClient();
-      final clientState = MockClientState();
-      final channel = MockChannel();
-      final channelState = MockChannelState();
-      _setupChannel(client, clientState, channel, channelState);
 
-      return _buildRecorderButtonScaffold(
+      final lockedState = RecordStateRecordingLocked(
+        duration: const Duration(seconds: 12),
+        waveform: List.generate(20, (i) => (i % 5) / 5.0),
+      );
+
+      return _buildVoiceRecorderScaffold(
         client: client,
-        channel: channel,
-        recordState: RecordStateRecordingLocked(
-          duration: const Duration(seconds: 12),
-          waveform: List.generate(20, (i) => (i % 5) / 5.0),
+        child: MessageComposerRecordingLocked(
+          audioRecorderController: _makeRecorderController(lockedState),
+          feedback: const AudioRecorderFeedback(),
+          messageInputController: StreamMessageInputController(),
+          sendMessageCallback: null,
+          state: lockedState,
         ),
       );
     },
@@ -180,46 +178,31 @@ void main() {
   goldenTest(
     'voice recording finished state',
     fileName: 'voice_recording_finished',
-    constraints: const BoxConstraints.tightFor(width: 375, height: 80),
+    constraints: const BoxConstraints.tightFor(width: 375, height: 120),
     builder: () {
       final client = MockClient();
-      final clientState = MockClientState();
-      final channel = MockChannel();
-      final channelState = MockChannelState();
-      _setupChannel(client, clientState, channel, channelState);
 
-      return _buildRecorderButtonScaffold(
-        client: client,
-        channel: channel,
-        recordState: RecordStateStopped(
-          audioRecording: Attachment(
-            type: 'voiceRecording',
-            assetUrl: 'https://example.com/recording.m4a',
-            extraData: const {
-              'duration': 15.0,
-              'waveform_data': <double>[0.1, 0.5, 0.9, 0.4, 0.2],
-            },
-          ),
+      final stoppedState = RecordStateStopped(
+        audioRecording: Attachment(
+          type: 'voiceRecording',
+          assetUrl: 'https://example.com/recording.m4a',
+          uploadState: const UploadState.success(),
+          extraData: const {
+            'duration': 15.0,
+            'waveform_data': <double>[0.1, 0.5, 0.9, 0.4, 0.2],
+          },
         ),
       );
-    },
-  );
 
-  goldenTest(
-    'voice recording stopped state',
-    fileName: 'voice_recording_stopped',
-    constraints: const BoxConstraints.tightFor(width: 375, height: 80),
-    builder: () {
-      final client = MockClient();
-      final clientState = MockClientState();
-      final channel = MockChannel();
-      final channelState = MockChannelState();
-      _setupChannel(client, clientState, channel, channelState);
-
-      return _buildRecorderButtonScaffold(
+      return _buildVoiceRecorderScaffold(
         client: client,
-        channel: channel,
-        recordState: const RecordStateIdle(),
+        child: MessageComposerRecordingStopped(
+          audioRecorderController: _makeRecorderController(stoppedState),
+          feedback: const AudioRecorderFeedback(),
+          messageInputController: StreamMessageInputController(),
+          sendMessageCallback: null,
+          recordingState: stoppedState,
+        ),
       );
     },
   );
