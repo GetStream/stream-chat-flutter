e9753090f81d65e653a7a19acf72a449f73fc915
Alexey Alter-Pesotskiy <alex@testableapple.com>
2026-07-15 11:28:54 +0100

    Update stuff

--- sample_app/integration_test/message_list_test.dart
+++ sample_app/integration_test/message_list_test.dart
@@ -12,13 +12,12 @@ void main() {
     description: 'message list updates when the user sends a message',
     body: (env) async {
       await step('GIVEN the user opens a channel', () async {
-        await env.backendRobot.generateChannels(channelsCount: 1);
         await env.userRobot.login();
         await env.userRobot.openChannel();
       });
 
-      await step('WHEN the user sends a message', () async {
-        await env.userRobot.sendMessage(sampleText);
+      await step('WHEN the participant sends a message', () async {
+        await env.participantRobot.sendMessage(sampleText);
       });
 
       await step('THEN the message is displayed', () async {
