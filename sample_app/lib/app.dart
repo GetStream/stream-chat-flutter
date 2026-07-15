1bca854375d51d93b47018a2e1278f297057741a
Rene Floor <rene.floor@getstream.io>
2026-07-15 14:58:42 +0200

    fix accentNeutral color

--- sample_app/lib/app.dart
+++ sample_app/lib/app.dart
@@ -272,6 +272,7 @@ class _StreamChatSampleAppState extends State<StreamChatSampleApp>
     return base.copyWith(
       // Accent
       accentError: dynamicColor.error,
+      accentNeutral: dynamicColor.secondary,
       // // Text
       textPrimary: dynamicColor.onSurface,
       textSecondary: dynamicColor.onSurfaceVariant,
