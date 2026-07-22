2642e3fec949a55169403e843a915c2d10c5553e
Rene Floor <rene.floor@getstream.io>
2026-07-22 10:53:56 +0200

    format

--- sample_app/lib/utils/firebase_support.dart
+++ sample_app/lib/utils/firebase_support.dart
@@ -8,7 +8,4 @@ import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 /// `NotificationService` skips FCM setup for the same reason. It remains
 /// enabled on web, Android, iOS, and macOS.
 bool get isFirebaseSupported =>
-    CurrentPlatform.isWeb ||
-    CurrentPlatform.isAndroid ||
-    CurrentPlatform.isIos ||
-    CurrentPlatform.isMacOS;
+    CurrentPlatform.isWeb || CurrentPlatform.isAndroid || CurrentPlatform.isIos || CurrentPlatform.isMacOS;
